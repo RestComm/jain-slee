c3b8ea8f45ae0e2e20c610b5200d23f753a226e8
EMMartins <EMMartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-05-29 15:37:02 +0000

    duhhh
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@1843 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- resources/parlay/provider-api/src/main/java/org/csapi/pam/PAM_MAX_LONGINT.java
+++
@@ -1,9 +0,0 @@
-package org.csapi.pam;
-/**
- * Automatically generated from IDL const definition 
- * @author JacORB IDL compiler 
- */
-
-public interface PAM_MAX_LONGINT
-{
-	long value = 2147483647;}
