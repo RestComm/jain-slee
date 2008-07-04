2e6c75c617e048607e7196c44dba28975fce0359
lbbbarreiro <lbbbarreiro@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-07-04 19:55:50 +0000

    Mobicents got stricter.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@2209 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- examples/sip-loadtest-without-dialogs/sbb/src/main/java/org/mobicents/slee/example/loadtest/SimpleCallSetupTerminatedByServerTestChildSbbLocalObject.java
+++ examples/sip-loadtest-without-dialogs/sbb/src/main/java/org/mobicents/slee/example/loadtest/SimpleCallSetupTerminatedByServerTestChildSbbLocalObject.java
@@ -1,8 +1,9 @@
 package org.mobicents.slee.example.loadtest;
 
 import javax.sip.message.Request;
+import javax.slee.SbbLocalObject;
 
-public interface SimpleCallSetupTerminatedByServerTestChildSbbLocalObject {
+public interface SimpleCallSetupTerminatedByServerTestChildSbbLocalObject extends SbbLocalObject {
 
 	public void setBye(Request request);
 	
