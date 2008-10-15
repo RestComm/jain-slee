592c42f9ec2aadb15d10a614cf6e1a65196c6d5c
brainslog <brainslog@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-10-15 15:21:15 +0000

    Removing Diameter out of JAIN-SLEE tree.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@3247 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- resources/diameter/jdiameter/api/src/main/java/org/jdiameter/api/sh/events/PushNotificationRequest.java
+++
@@ -1,8 +0,0 @@
-package org.jdiameter.api.sh.events;
-
-import org.jdiameter.api.app.AppRequestEvent;
-
-public interface PushNotificationRequest extends AppRequestEvent {
-	public static final String _SHORT_NAME="PNR";
-	public static final String _LONG_NAME="Push-Notification-Request";
-}
--- resources/diameter/jdiameter/api/src/main/java/org/jdiameter/api/sh/events/UserDataAnswer.java
+++
@@ -1,8 +0,0 @@
-package org.jdiameter.api.sh.events;
-
-import org.jdiameter.api.app.AppAnswerEvent;
-
-public interface UserDataAnswer extends AppAnswerEvent {
-	public static final String _SHORT_NAME="UDA";
-	public static final String _LONG_NAME="User-Data-Answer";
-}
