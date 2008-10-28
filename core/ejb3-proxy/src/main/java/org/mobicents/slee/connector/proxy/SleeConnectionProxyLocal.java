06e67126a24ac957e57e7991b5dbe222ae692a54
EMMartins <EMMartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-10-28 00:34:30 +0000

    patch 1 of 409
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@3382 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- core/ejb3-proxy/src/main/java/org/mobicents/slee/connector/proxy/SleeConnectionProxyLocal.java
+++
@@ -1,6 +0,0 @@
-package org.mobicents.slee.connector.proxy;
-
-@javax.ejb.Local
-public interface SleeConnectionProxyLocal extends SleeConnectionProxyInterface{
-
-}
--- core/ejb3-proxy/src/main/java/org/mobicents/slee/connector/proxy/SleeConnectionProxyRemote.java
+++
@@ -1,6 +0,0 @@
-package org.mobicents.slee.connector.proxy;
-
-@javax.ejb.Remote
-public interface SleeConnectionProxyRemote extends SleeConnectionProxyInterface{
-
-}
