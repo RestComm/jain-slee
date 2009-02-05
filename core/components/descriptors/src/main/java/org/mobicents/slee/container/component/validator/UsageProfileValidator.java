accf59a160bbd16e3ab7ed1f1c9ad0d36bce2a05
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-02-05 19:44:47 +0000

    draft class for usage parameters validation
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@4714 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- core/components/descriptors/src/main/java/org/mobicents/slee/container/component/validator/UsageProfileValidator.java
+++ core/components/descriptors/src/main/java/org/mobicents/slee/container/component/validator/UsageProfileValidator.java
@@ -16,21 +16,46 @@ import org.mobicents.slee.container.component.deployment.jaxb.descriptors.common
  * Start time:19:32:15 2009-02-05<br>
  * Project: mobicents-jainslee-server-core<br>
  * 
- * This class is place where common elements like usage parameter interface is validated. In 1.1 specs its widely usable - ra, profiles, sbbs. In 1.0 only sbbs have those.
+ * This class is place where common elements like usage parameter interface is
+ * validated. In 1.1 specs its widely usable - ra, profiles, sbbs. In 1.0 only
+ * sbbs have those.
+ * 
  * @author <a href="mailto:baranowb@gmail.com">baranowb - Bartosz Baranowski
  *         </a>
  * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a>
  */
 public class UsageProfileValidator {
 
-	
-	
-	public static boolean validateUsageParameterInterface(Class usageComponent,Class usageInterface, List<MUsageParameter> parameters)
-	{
-		
+	/**
+	 * This methods validate component which has usage parameter interface. Its
+	 * interface for methods. In case of 1.1 components parameters list must
+	 * match evey method defined. In case of 1.0 components parameters list MUST
+	 * be null. It does not validate get usage method, those
+	 * 
+	 * @param usageInterface - interface class itself
+	 * @param parameters - list of parameters, in case of 1.0 sbb this MUST be null.
+	 * @return
+	 */
+	static boolean validateUsageParameterInterface(
+			Class usageInterface, List<MUsageParameter> parameters) {
+
+		return false;
+	}
+
+	static boolean validateSbbUsageParameterInterface(Class usageComponent,
+			Class usageInterface, List<MUsageParameter> parameters) {
+
 		return false;
 	}
 	
-	
-	
+	static boolean validateResourceAdaptorUsageParameterInterface(Class usageComponent,
+			Class usageInterface, List<MUsageParameter> parameters) {
+
+		return false;
+	}
+	static boolean validateprofileSpecificationUsageParameterInterface(Class usageComponent,
+			Class usageInterface, List<MUsageParameter> parameters) {
+
+		return false;
+	}
 }
