6d11b600edbc4e742df1f2ebee3131aebea954d9
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-04-06 12:52:21 +0000

    bugs
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@5311 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- core/jar/src/main/java/org/mobicents/slee/resource/AbstractActivityContextInterfaceFactory.java
+++ core/jar/src/main/java/org/mobicents/slee/resource/AbstractActivityContextInterfaceFactory.java
@@ -1,5 +1,7 @@
 package org.mobicents.slee.resource;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 
 import javax.slee.ActivityContextInterface;
@@ -18,7 +20,7 @@ import org.mobicents.slee.runtime.activity.ActivityContextInterfaceImpl;
  * Implementation of the abstract resource adaptor aci factory code.
  * 
  * @author martins
- *
+ * 
  */
 public class AbstractActivityContextInterfaceFactory implements Serializable {
 
@@ -26,57 +28,74 @@ public class AbstractActivityContextInterfaceFactory implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	private final SleeContainer sleeContainer;
+
+	private transient SleeContainer sleeContainer;
 	private final ResourceAdaptorTypeID resourceAdaptorTypeID;
-	
-	protected AbstractActivityContextInterfaceFactory(SleeContainer sleeContainer,
+
+	protected AbstractActivityContextInterfaceFactory(
+			SleeContainer sleeContainer,
 			ResourceAdaptorTypeID resourceAdaptorTypeID) {
 		this.sleeContainer = sleeContainer;
 		this.resourceAdaptorTypeID = resourceAdaptorTypeID;
 	}
-	
+
 	/**
-	 * This method returns the aci for the specified activity, if exists, it should be invoked by each impl of methods of an ra type aci factory. 
+	 * This method returns the aci for the specified activity, if exists, it
+	 * should be invoked by each impl of methods of an ra type aci factory.
+	 * 
 	 * @param activity
 	 * @return
 	 * @throws NullPointerException
 	 * @throws UnrecognizedActivityException
 	 * @throws FactoryException
 	 */
-	protected ActivityContextInterface getACI(Object activity) throws NullPointerException,
-	UnrecognizedActivityException, FactoryException {
+	protected ActivityContextInterface getACI(Object activity)
+			throws NullPointerException, UnrecognizedActivityException,
+			FactoryException {
 		if (activity == null) {
 			throw new NullPointerException("null activity object");
 		}
 		ActivityHandle handle = null;
-		for (ResourceAdaptorEntity raEntity : sleeContainer.getResourceManagement().getResourceAdaptorEntitiesPerType().get(resourceAdaptorTypeID)) {
-			handle = raEntity.getResourceAdaptorObject().getActivityHandle(activity);
+		for (ResourceAdaptorEntity raEntity : sleeContainer
+				.getResourceManagement().getResourceAdaptorEntitiesPerType()
+				.get(resourceAdaptorTypeID)) {
+			handle = raEntity.getResourceAdaptorObject().getActivityHandle(
+					activity);
 			if (handle != null) {
-				ActivityContextHandle ach = ActivityContextHandlerFactory.createExternalActivityContextHandle(raEntity.getName(), handle);
-				ActivityContext ac = sleeContainer.getActivityContextFactory().getActivityContext(ach, true);
+				ActivityContextHandle ach = ActivityContextHandlerFactory
+						.createExternalActivityContextHandle(
+								raEntity.getName(), handle);
+				ActivityContext ac = sleeContainer.getActivityContextFactory()
+						.getActivityContext(ach, true);
 				if (ac != null) {
 					return new ActivityContextInterfaceImpl(ac);
 				}
 				break;
 			}
 		}
-		
+
 		throw new UnrecognizedActivityException(activity);
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return resourceAdaptorTypeID.hashCode();
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (obj != null && obj.getClass() == this.getClass()) {
-			return ((AbstractActivityContextInterfaceFactory)obj).resourceAdaptorTypeID.equals(this.resourceAdaptorTypeID);
-		}
-		else {
+			return ((AbstractActivityContextInterfaceFactory) obj).resourceAdaptorTypeID
+					.equals(this.resourceAdaptorTypeID);
+		} else {
 			return false;
 		}
 	}
+
+	private void readObject(ObjectInputStream is)
+			throws ClassNotFoundException, IOException {
+		is.defaultReadObject();
+		sleeContainer = SleeContainer.lookupFromJndi();
+	}
+
 }
