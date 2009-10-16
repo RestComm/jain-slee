1f72aba0479815f216c3b1e22e3c4d3b0dd8b506
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-10-16 21:24:20 +0000

    enhancing sip load test in slee 2.x, adding sipp scenarios
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@8070 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- examples/sip11-loadtest-with-dialogs/sbb/src/main/java/org/mobicents/slee/example/loadtest/sip11/SimpleCallSetupTerminatedByServerWithDialogsTestSbb.java
+++ examples/sip11-loadtest-with-dialogs/sbb/src/main/java/org/mobicents/slee/example/loadtest/sip11/SimpleCallSetupTerminatedByServerWithDialogsTestSbb.java
@@ -1,28 +1,22 @@
 package org.mobicents.slee.example.loadtest.sip11;
 
-import gov.nist.javax.sip.Utils;
-
 import java.text.ParseException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.sip.ClientTransaction;
 import javax.sip.ListeningPoint;
-import javax.sip.RequestEvent;
 import javax.sip.ServerTransaction;
-import javax.sip.address.Address;
 import javax.sip.address.AddressFactory;
-import javax.sip.address.SipURI;
 import javax.sip.header.ContactHeader;
 import javax.sip.header.HeaderFactory;
-import javax.sip.header.ToHeader;
 import javax.sip.message.MessageFactory;
 import javax.sip.message.Request;
 import javax.sip.message.Response;
 import javax.slee.ActivityContextInterface;
 import javax.slee.RolledBackContext;
 import javax.slee.SbbContext;
+import javax.slee.SbbLocalObject;
 import javax.slee.facilities.TimerEvent;
 import javax.slee.facilities.TimerFacility;
 import javax.slee.facilities.TimerOptions;
@@ -42,56 +36,55 @@ public abstract class SimpleCallSetupTerminatedByServerWithDialogsTestSbb implem
     private TimerFacility timerFacility;
     
     private static ContactHeader contactHeader;
+    private static TimerOptions timerOptions;
     
     private ContactHeader getContactHeader() throws ParseException {
     	if (contactHeader == null) {
-    		ListeningPoint listeningPoint = sipFactoryProvider
-			.getListeningPoint("udp");
-			Address address = addressFactory.createAddress(
-					"Mobicents SIP AS <sip:"+listeningPoint.getIPAddress()+">");
-			((SipURI) address.getURI()).setPort(listeningPoint.getPort());
-			contactHeader = headerFactory.createContactHeader(address);
+    		final ListeningPoint listeningPoint = sipFactoryProvider.getListeningPoint("udp");
+    		final javax.sip.address.SipURI sipURI = addressFactory.createSipURI(null, listeningPoint.getIPAddress());
+			sipURI.setPort(listeningPoint.getPort());			
+			sipURI.setTransportParam(listeningPoint.getTransport());
+			contactHeader = headerFactory.createContactHeader(addressFactory.createAddress(sipURI));			
     	}
     	return contactHeader;
     }
     
+    private TimerOptions getTimerOptions() {
+    	if (timerOptions == null) {
+    		timerOptions = new TimerOptions();
+    		timerOptions.setPreserveMissed(TimerPreserveMissed.ALL);
+    	}
+    	return timerOptions;
+    }
+    
 	public void onInviteEvent(javax.sip.RequestEvent requestEvent, ActivityContextInterface aci) {
+		final SbbLocalObject sbbLocalObject = this.sbbContext.getSbbLocalObject();
+		final ServerTransaction serverTransaction = requestEvent.getServerTransaction();
 		try {
-			ServerTransaction serverTransaction = requestEvent.getServerTransaction();
 			// send 100
 			// serverTransaction.sendResponse(messageFactory.createResponse(100,requestEvent.getRequest()));
 			// create dialog and attach this entity to it's aci
-			DialogActivity dialog = (DialogActivity) sipFactoryProvider.getNewDialog(serverTransaction);
-			sipActivityContextInterfaceFactory.getActivityContextInterface(dialog).attach(this.sbbContext.getSbbLocalObject());
+			final DialogActivity dialog = (DialogActivity) sipFactoryProvider.getNewDialog(serverTransaction);
+			final ActivityContextInterface dialogAci = sipActivityContextInterfaceFactory.getActivityContextInterface(dialog);
+			dialogAci.attach(sbbLocalObject);
+			// set timer of 60 secs on the dialog aci
+			timerFacility.setTimer(dialogAci, null, System.currentTimeMillis()+60000, getTimerOptions());
 			// send 200 ok
-			Response response = messageFactory
+			final Response response = messageFactory
 					.createResponse(Response.OK,requestEvent.getRequest());
 			response.addHeader(getContactHeader());
-			((ToHeader)response.getHeader(ToHeader.NAME)).setTag(Utils.getInstance().generateTag());
-			serverTransaction.sendResponse(response);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	public void onAckEvent(RequestEvent requestEvent, ActivityContextInterface aci) {
-		try {
-			// set timer of 60 secs
-            TimerOptions timerOptions = new TimerOptions();
-            timerOptions.setPreserveMissed(TimerPreserveMissed.ALL);
-            timerFacility.setTimer(aci, null, System.currentTimeMillis()+60000, timerOptions);
+			serverTransaction.sendResponse(response);			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 	
 	public void onTimerEvent(TimerEvent event, ActivityContextInterface aci) {
+		final DialogActivity dialog = (DialogActivity)aci.getActivity();
 		try {
-			DialogActivity dialog = (DialogActivity)aci.getActivity(); 
-			Request request = dialog.createRequest(Request.BYE);
+			final Request request = dialog.createRequest(Request.BYE);
 			request.addHeader(getContactHeader());
-			ClientTransaction clientTransaction = sipFactoryProvider.getNewClientTransaction(request);
-			clientTransaction.sendRequest();
+			dialog.sendRequest(request);			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -100,7 +93,7 @@ public abstract class SimpleCallSetupTerminatedByServerWithDialogsTestSbb implem
 	public void setSbbContext(SbbContext context) { 
 		this.sbbContext = context; 
 		try {
-			Context ctx = (Context)new InitialContext().lookup("java:comp/env");
+			final Context ctx = (Context)new InitialContext().lookup("java:comp/env");
 			sipActivityContextInterfaceFactory = (SipActivityContextInterfaceFactory)ctx.lookup("slee/resources/jainsip/1.2/acifactory");
 			sipFactoryProvider = (SleeSipProvider)ctx.lookup("slee/resources/jainsip/1.2/provider");
 	        addressFactory = sipFactoryProvider.getAddressFactory();
@@ -123,8 +116,6 @@ public abstract class SimpleCallSetupTerminatedByServerWithDialogsTestSbb implem
     public void sbbStore() {}
     public void sbbExceptionThrown(Exception exception, Object event, ActivityContextInterface activity) {}
     public void sbbRolledBack(RolledBackContext context) {}
-	
-
 	
 	/**
 	 * Convenience method to retrieve the SbbContext object stored in setSbbContext.
