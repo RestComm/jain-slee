c9b65bd6136f5f6e1624bb27f3e820965ba6bfb5
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-01-20 00:37:02 +0000

    fixes and optimizations related with sip b2bua perf
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@9545 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- examples/sip-b2bua/sbb/src/main/java/org/mobicents/slee/example/sip11/b2b/SimpleSip11B2BTestSbb.java
+++ examples/sip-b2bua/sbb/src/main/java/org/mobicents/slee/example/sip11/b2b/SimpleSip11B2BTestSbb.java
@@ -4,6 +4,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sip.ClientTransaction;
+import javax.sip.DialogState;
 import javax.sip.InvalidArgumentException;
 import javax.sip.RequestEvent;
 import javax.sip.ResponseEvent;
@@ -25,25 +26,36 @@ import net.java.slee.resource.sip.SleeSipProvider;
 
 public abstract class SimpleSip11B2BTestSbb implements javax.slee.Sbb {
 
-	private SbbContext sbbContext; 
+	private SbbContext sbbContext;
 	private static Tracer tracer;
-	
+
 	private SipActivityContextInterfaceFactory sipActivityContextInterfaceFactory;
 	private SleeSipProvider sipProvider;
-	
+
 	// Initial request
 	public void onInviteEvent(RequestEvent event, ActivityContextInterface aci) {
+
+		if (getIncomingDialog() != null) {
+			tracer.severe("Repeated request: " + event.getRequest());
+			return;
+		}
+
 		// ACI is the server transaction activity
 		final ServerTransaction st = event.getServerTransaction();
 		try {
 			// Create the dialogs representing the incoming and outgoing call
 			// legs.
-			final DialogActivity incomingDialog = (DialogActivity) sipProvider.getNewDialog(st);
-			final DialogActivity outgoingDialog = sipProvider.getNewDialog(incomingDialog, false);
+			final DialogActivity incomingDialog = (DialogActivity) sipProvider
+					.getNewDialog(st);
+			final DialogActivity outgoingDialog = sipProvider.getNewDialog(
+					incomingDialog, true);
 			// Obtain the dialog activity contexts and attach to them
-			final ActivityContextInterface outgoingDialogACI = sipActivityContextInterfaceFactory.getActivityContextInterface(outgoingDialog);
-			final ActivityContextInterface incomingDialogACI = sipActivityContextInterfaceFactory.getActivityContextInterface(incomingDialog);
-			final SbbLocalObject sbbLocalObject = sbbContext.getSbbLocalObject();
+			final ActivityContextInterface outgoingDialogACI = sipActivityContextInterfaceFactory
+					.getActivityContextInterface(outgoingDialog);
+			final ActivityContextInterface incomingDialogACI = sipActivityContextInterfaceFactory
+					.getActivityContextInterface(incomingDialog);
+			final SbbLocalObject sbbLocalObject = sbbContext
+					.getSbbLocalObject();
 			incomingDialogACI.attach(sbbLocalObject);
 			outgoingDialogACI.attach(sbbLocalObject);
 			// Record which dialog is which, so we can find the peer dialog
@@ -52,135 +64,162 @@ public abstract class SimpleSip11B2BTestSbb implements javax.slee.Sbb {
 			setOutgoingDialog(outgoingDialogACI);
 			forwardRequest(st, outgoingDialog);
 		} catch (Throwable e) {
-			tracer.severe("Failed to process incoming INVITE.",e);
-			sendErrorResponse(st, Response.SERVICE_UNAVAILABLE);
+			tracer.severe("Failed to process incoming INVITE.", e);
+			replyToRequestEvent(event, Response.SERVICE_UNAVAILABLE);
 		}
 	}
 
 	// Responses
 	public void on1xxResponse(ResponseEvent event, ActivityContextInterface aci) {
-		processResponse(event, aci);
-	}
-
-	public void on2xxResponse(ResponseEvent event, ActivityContextInterface aci) {
-		if(((CSeqHeader)event.getResponse().getHeader(CSeqHeader.NAME)).getMethod().equals(Request.CANCEL)) {
-			//TODO: add ack on 487
+		if (event.getResponse().getStatusCode() == Response.TRYING) {
+			// those are not forwarded to the other dialog
 			return;
 		}
 		processResponse(event, aci);
 	}
 
-	// other responses handled the same way as above
-	// Mid-dialog requests
-	public void onAck(RequestEvent event, ActivityContextInterface aci) {
-		processMidDialogRequest(event, aci);
+	public void on2xxResponse(ResponseEvent event, ActivityContextInterface aci) {
+		final CSeqHeader cseq = (CSeqHeader) event.getResponse().getHeader(
+				CSeqHeader.NAME);
+		if (cseq.getMethod().equals(Request.INVITE)) {
+			// lets ack it ourselves to avoid UAS retransmissions due to
+			// forwarding of this response and further UAC Ack
+			// note that the app does not handles UAC ACKs
+			try {
+				final Request ack = event.getDialog().createAck(
+						cseq.getSeqNumber());
+				event.getDialog().sendAck(ack);
+			} catch (Exception e) {
+				tracer.severe("Unable to ack INVITE's 200 ok from UAS", e);
+			}
+		} else if (cseq.getMethod().equals(Request.BYE)
+				|| cseq.getMethod().equals(Request.CANCEL)) {
+			// not forwarded to the other dialog
+			return;
+		}
+		processResponse(event, aci);
 	}
 
 	public void onBye(RequestEvent event, ActivityContextInterface aci) {
+		// send back 200 ok for this dialog right away, to avoid retransmissions
+		replyToRequestEvent(event, Response.OK);
+		// forward to the other dialog
 		processMidDialogRequest(event, aci);
 	}
 
 	public void onCancel(CancelRequestEvent event, ActivityContextInterface aci) {
-		this.sipProvider.acceptCancel(event, false);
-		processMidDialogRequest(event, aci);
+		if (tracer.isInfoEnabled()) {
+			tracer.info("Got a CANCEL request.");
+		}
+		
+		try {
+			this.sipProvider.acceptCancel(event, false);
+			final ActivityContextInterface peerDialogACI = getPeerDialog(aci);
+			final DialogActivity peerDialog = (DialogActivity) peerDialogACI
+					.getActivity();
+			final DialogState peerDialogState = peerDialog.getState();
+			if (peerDialogState == null || peerDialogState == DialogState.EARLY) {
+				peerDialog.sendCancel();
+			} else {
+				peerDialog.sendRequest(peerDialog.createRequest(Request.BYE));
+			}
+		} catch (Exception e) {
+			tracer.severe("Failed to process cancel request", e);
+		}
 	}
-	
+
 	// Other mid-dialog requests handled the same way as above
 	// Helpers
-	private void processMidDialogRequest(RequestEvent event, ActivityContextInterface dialogACI) {
+
+	private void replyToRequestEvent(RequestEvent event, int status) {
+		try {
+			event.getServerTransaction().sendResponse(
+					sipProvider.getMessageFactory().createResponse(status,
+							event.getRequest()));
+		} catch (Throwable e) {
+			tracer.severe("Failed to reply to request event:\n" + event, e);
+		}
+	}
+
+	private void processMidDialogRequest(RequestEvent event,
+			ActivityContextInterface dialogACI) {
 		try {
 			// Find the dialog to forward the request on
 			ActivityContextInterface peerACI = getPeerDialog(dialogACI);
-			forwardRequest(event.getServerTransaction(), (DialogActivity) peerACI.getActivity());
+			forwardRequest(event.getServerTransaction(),
+					(DialogActivity) peerACI.getActivity());
 		} catch (SipException e) {
-			tracer.severe(e.getMessage(),e);
-			sendErrorResponse(event.getServerTransaction(), Response.SERVICE_UNAVAILABLE);
+			tracer.severe(e.getMessage(), e);
+			replyToRequestEvent(event, Response.SERVICE_UNAVAILABLE);
 		}
 	}
 
-	private void processResponse(ResponseEvent event, ActivityContextInterface aci) {
+	private void processResponse(ResponseEvent event,
+			ActivityContextInterface aci) {
 		try {
 			// Find the dialog to forward the response on
 			ActivityContextInterface peerACI = getPeerDialog(aci);
-			forwardResponse((DialogActivity) aci.getActivity(), (DialogActivity) peerACI.getActivity(), event.getClientTransaction(), event.getResponse());
+			forwardResponse((DialogActivity) aci.getActivity(),
+					(DialogActivity) peerACI.getActivity(), event
+							.getClientTransaction(), event.getResponse());
 		} catch (SipException e) {
-			tracer.severe(e.getMessage(),e);
+			tracer.severe(e.getMessage(), e);
 		}
 	}
 
-	private ActivityContextInterface getPeerDialog(ActivityContextInterface aci) throws SipException {
+	private ActivityContextInterface getPeerDialog(ActivityContextInterface aci)
+			throws SipException {
 		final ActivityContextInterface incomingDialogAci = getIncomingDialog();
-		final DialogActivity incomingDialog = (DialogActivity) incomingDialogAci.getActivity();
-		if ( aci.getActivity().equals(incomingDialog)) {	
-			if (tracer.isInfoEnabled()) {
-				tracer.info("Peer is outgoing dialog.");
-			}
+		if (aci.equals(incomingDialogAci)) {
 			return getOutgoingDialog();
 		}
-		final DialogActivity outgoingDialog = (DialogActivity) getOutgoingDialog().getActivity();
-		if (aci.getActivity().equals(outgoingDialog)) {	
-			if (tracer.isInfoEnabled()) {
-				tracer.info("Peer is incoming dialog.");
-			}
+		if (aci.equals(getOutgoingDialog())) {
 			return incomingDialogAci;
 		}
 		throw new SipException("could not find peer dialog");
+
 	}
 
-	private void forwardRequest(ServerTransaction st, DialogActivity out) throws SipException {
+	private void forwardRequest(ServerTransaction st, DialogActivity out)
+			throws SipException {
 		final Request incomingRequest = st.getRequest();
 		if (tracer.isInfoEnabled()) {
-			tracer.info("Forwarding request "+incomingRequest.getMethod()+" to dialog "+out);
+			tracer.info("Forwarding request " + incomingRequest.getMethod()
+					+ " to dialog " + out);
 		}
 		// Copies the request, setting the appropriate headers for the dialog.
-		if (incomingRequest.getMethod().equals(Request.ACK)) {
-			// Just forward the ACK statelessly - don't need to remember
-			// transaction state
-			final Request outgoingRequest = out.createRequest(incomingRequest);
-			out.sendAck(outgoingRequest);
-		} 
-		else if(incomingRequest.getMethod().equals(Request.CANCEL)) {
-			out.sendCancel();
-		}
-		else {
-			final Request outgoingRequest = out.createRequest(incomingRequest);
-			// Send the request on the dialog activity
-			final ClientTransaction ct = out.sendRequest(outgoingRequest);
-			// Record an association with the original server transaction,
-			// so we can retrieve it when forwarding the response.
-			out.associateServerTransaction(ct, st);
-		}
+		Request outgoingRequest = out.createRequest(incomingRequest);
+		// Send the request on the dialog activity
+		final ClientTransaction ct = out.sendRequest(outgoingRequest);
+		// Record an association with the original server transaction,
+		// so we can retrieve it when forwarding the response.
+		out.associateServerTransaction(ct, st);
 	}
 
-	private void forwardResponse(DialogActivity in, DialogActivity out, ClientTransaction ct, Response receivedResponse) throws SipException {
+	private void forwardResponse(DialogActivity in, DialogActivity out,
+			ClientTransaction ct, Response receivedResponse)
+			throws SipException {
 		// Find the original server transaction that this response
 		// should be forwarded on.
 		final ServerTransaction st = in.getAssociatedServerTransaction(ct);
 		// could be null
 		if (st == null)
-			throw new SipException("could not find associated server transaction");
+			throw new SipException(
+					"could not find associated server transaction");
 		if (tracer.isInfoEnabled()) {
-			tracer.info("Forwarding response "+receivedResponse.getStatusCode()+" to dialog "+out);
+			tracer.info("Forwarding response "
+					+ receivedResponse.getStatusCode() + " to dialog " + out);
 		}
 		// Copy the response across, setting the appropriate headers for the
 		// dialog
-		final Response outgoingResponse = out.createResponse(st, receivedResponse);
+		final Response outgoingResponse = out.createResponse(st,
+				receivedResponse);
 		// Forward response upstream.
 		try {
 			st.sendResponse(outgoingResponse);
 		} catch (InvalidArgumentException e) {
-			tracer.severe(e.getMessage(),e);
+			tracer.severe("Failed to send response:\n" + outgoingResponse, e);
 			throw new SipException("invalid response", e);
-		}
-	}
-
-	private void sendErrorResponse(ServerTransaction st, int statusCode) {
-		try {
-			final Response response = sipProvider.getMessageFactory().createResponse(statusCode, st.getRequest());
-			st.sendResponse(response);
-		} catch (Exception e) {
-			// exception handling code
-			tracer.severe(e.getMessage(),e);
 		}
 	}
 
@@ -197,15 +236,19 @@ public abstract class SimpleSip11B2BTestSbb implements javax.slee.Sbb {
 
 	public void setSbbContext(SbbContext context) {
 		this.sbbContext = context;
-		if (tracer == null){
-			tracer = sbbContext.getTracer(SimpleSip11B2BTestSbb.class.getSimpleName());
+		if (tracer == null) {
+			tracer = sbbContext.getTracer(SimpleSip11B2BTestSbb.class
+					.getSimpleName());
 		}
 		try {
-			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
-			sipActivityContextInterfaceFactory = (SipActivityContextInterfaceFactory) ctx.lookup("slee/resources/jainsip/1.2/acifactory");
-			sipProvider = (SleeSipProvider) ctx.lookup("slee/resources/jainsip/1.2/provider");
+			Context ctx = (Context) new InitialContext()
+					.lookup("java:comp/env");
+			sipActivityContextInterfaceFactory = (SipActivityContextInterfaceFactory) ctx
+					.lookup("slee/resources/jainsip/1.2/acifactory");
+			sipProvider = (SleeSipProvider) ctx
+					.lookup("slee/resources/jainsip/1.2/provider");
 		} catch (NamingException e) {
-			tracer.severe(e.getMessage(),e);
+			tracer.severe(e.getMessage(), e);
 		}
 	}
 
@@ -234,7 +277,8 @@ public abstract class SimpleSip11B2BTestSbb implements javax.slee.Sbb {
 	public void sbbStore() {
 	}
 
-	public void sbbExceptionThrown(Exception exception, Object event, ActivityContextInterface activity) {
+	public void sbbExceptionThrown(Exception exception, Object event,
+			ActivityContextInterface activity) {
 	}
 
 	public void sbbRolledBack(RolledBackContext context) {
