2d73d25c2debe3f42b79dae3f011c62902bb8539
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-02-11 03:07:47 +0000

    Fixes issue 1230
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@9969 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- examples/google-talk-bot/sbb/src/main/java/org/mobicents/examples/googletalk/GoogleTalkBotSbb.java
+++ examples/google-talk-bot/sbb/src/main/java/org/mobicents/examples/googletalk/GoogleTalkBotSbb.java
@@ -17,6 +17,8 @@ import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.util.StringUtils;
+import org.mobicents.slee.resource.xmpp.XmppActivityContextInterfaceFactory;
+import org.mobicents.slee.resource.xmpp.XmppConnection;
 import org.mobicents.slee.resource.xmpp.XmppResourceAdaptorSbbInterface;
 
 /**
@@ -25,207 +27,241 @@ import org.mobicents.slee.resource.xmpp.XmppResourceAdaptorSbbInterface;
  * @author Eduardo Martins
  * @version 2.0
  * 
- * A prototype SBB for the XMPP Resource Adaptor in client mode. It demonstrates
- * connection with the GoogleTalk service. Simulates a regular GoogleTalk user.
+ *          A prototype SBB for the XMPP Resource Adaptor in client mode. It
+ *          demonstrates connection with the GoogleTalk service. Simulates a
+ *          regular GoogleTalk user.
  * 
- * The bot simply receives IM chat messages from Google Talk users and echoes
- * them back. It also responds to a 'time' message with the current time at the
- * server where the SBB is hosted.
+ *          The bot simply receives IM chat messages from Google Talk users and
+ *          echoes them back. It also responds to a 'time' message with the
+ *          current time at the server where the SBB is hosted.
  * 
  */
 
 public abstract class GoogleTalkBotSbb implements javax.slee.Sbb {
 
-	private Class<?>[] packetsToListen = { Message.class, Presence.class };
+	private final static Class<?>[] packetsToListen = { Message.class,
+			Presence.class };
 
-	private String connectionID = "org.mobicents.examples.googletalk.GoogleTalkBotSbb";
+	private final static String connectionID = "org.mobicents.examples.googletalk.GoogleTalkBotSbb";
 
-	private String username = "CHANGE_UID";
+	private static final String resource = "MobicentsGoogleTalkBot";
 
-	private String password = "CHANGE_PASSWD";
+	private static final String serviceHost = "talk.google.com";
 
-	private String resource = "MobicentsGoogleTalkBot";
+	private static final int servicePort = 5222;
 
-	private String serviceHost = "talk.google.com";
-
-	private int servicePort = 5222;
-
-	private String serviceName = "gmail.com";
+	private static final String serviceName = "gmail.com";
 
 	private XmppResourceAdaptorSbbInterface xmppSbbInterface;
+	private XmppActivityContextInterfaceFactory xmppActivityContextInterfaceFactory;
 
-	private SbbContext sbbContext; // This SBB's SbbContext
+	private SbbContext sbbContext;
+	private Tracer tracer;
 
-	static private Tracer logger;
+	private String username;
+	private String password;
 
-	/*
+	/**
 	 * Init the xmpp connection to GOOGLE TALK when the service is activated by
 	 * SLEE
+	 * 
+	 * @param event
+	 * @param aci
 	 */
 	public void onStartServiceEvent(
 			javax.slee.serviceactivity.ServiceStartedEvent event,
 			ActivityContextInterface aci) {
 		try {
 			// connect to google talk xmpp server
-			xmppSbbInterface.connectClient(connectionID, serviceHost,
-					servicePort, serviceName, username, password, resource,
-					Arrays.asList(packetsToListen));
+			XmppConnection connection = xmppSbbInterface.connectClient(
+					connectionID, serviceHost, servicePort, serviceName,
+					username, password, resource, Arrays
+							.asList(packetsToListen));
+			xmppActivityContextInterfaceFactory.getActivityContextInterface(
+					connection).attach(sbbContext.getSbbLocalObject());
 		} catch (XMPPException e) {
-			e.printStackTrace();
-			logger.info("Connection to server failed! Error:" + e.getMessage());
+			tracer.severe("Connection to server failed!",e);
 		}
 	}
 
 	/**
-	 * Here we receive the Presence messages.
+	 * Here we handle the Presence messages.
+	 * 
+	 * @param packet
+	 * @param aci
 	 */
 	public void onPresence(org.jivesoftware.smack.packet.Presence packet,
 			ActivityContextInterface aci) {
-		// check if this event is "to" me
-		if (StringUtils.parseBareAddress(packet.getTo()).equals(
-				username + "@" + serviceName)) {
-			logger.info("XMPP Presence event type! Status: '" + packet.getType() + "'. "
-					+ "Sent by '" + packet.getFrom() + "'.");
-			// reply hello msg if receives notification of available presence state
-			if (packet.getType() == Presence.Type.AVAILABLE) {
-				Message msg = new Message(packet.getFrom(), Message.Type.CHAT);
-				msg.setBody("Hi. I'am online too.");
-				xmppSbbInterface.sendPacket(connectionID, msg);
-			}
+		tracer.info("XMPP Presence event type! Status: '" + packet.getType()
+				+ "'. " + "Sent by '" + packet.getFrom() + "'.");
+		// reply hello msg if receives notification of available presence state
+		if (packet.getType() == Presence.Type.AVAILABLE) {
+			Message msg = new Message(packet.getFrom(), Message.Type.CHAT);
+			msg.setBody("Hi. I'am online too.");
+			xmppSbbInterface.sendPacket(connectionID, msg);
 		}
 	}
 
 	/**
 	 * This is the point where we already have a chat session with the user, so,
 	 * when they send us messages, we count the chars and reply or tell time :)
+	 * 
+	 * @param message
+	 * @param aci
 	 */
 	public void onMessage(org.jivesoftware.smack.packet.Message message,
 			ActivityContextInterface aci) {
-		// check if this event is "to" me and it's not an error
-		if (StringUtils.parseBareAddress(message.getTo()).equals(
-				username + "@" + serviceName)
-				&& !message.getType().equals(Message.Type.ERROR)) {
-		  
-			logger.info("XMPP Message event type! Message Body: '" + message.getBody() + "'. "
-			    + "Sent by '" + message.getFrom() + "'.");
-			
+		// only process messages which are not an error and not sent by another
+		// bot instance
+		if (!message.getType().equals(Message.Type.ERROR)
+				&& !StringUtils.parseBareAddress(message.getFrom()).equals(
+						username + "@" + serviceName)) {
+			tracer.info("XMPP Message event type! Message Body: '"
+					+ message.getBody() + "'. " + "Sent by '"
+					+ message.getFrom() + "'.");
 			String body = null;
 			if (message.getBody() != null) {
-				if (message.getBody().toLowerCase().startsWith("time")) {
-					Date d = new Date();
-					body = "My system time is " + d.toString();
+				if (message.getBody().equalsIgnoreCase("time")) {
+					body = "My system time is " + new Date().toString();
 				} else {
 					body = message.getBody().length() + " chars in message <"
 							+ message.getBody() + ">.";
 				}
 			}
-      // Filter messages from other running bots! 
-      if(message.getFrom().startsWith( "mobicents.org@gmail.com" )) {
-        logger.info( "XMPP Message is from Bot '" + message.getFrom() + "'. Not replying!" );
-        return;
-      }
-			
 			Message msg = new Message(message.getFrom(), message.getType());
 			msg.setBody(body);
 			xmppSbbInterface.sendPacket(connectionID, msg);
 		}
 	}
 
-	/*
-	 * Handler to disconnect from Google when the service is being deactivated
+	/**
+	 * Handler to disconnect from Google when the service is being deactivated.
+	 * 
+	 * @param event
+	 * @param aci
 	 */
-
 	public void onActivityEndEvent(ActivityEndEvent event,
 			ActivityContextInterface aci) {
 		if (aci.getActivity() instanceof ServiceActivity) {
 			try {
 				xmppSbbInterface.disconnect(connectionID);
 			} catch (Exception e) {
-				logger.severe(e.getMessage(),e);
+				tracer.severe(e.getMessage(), e);
 			}
 		}
 	}
 
-	/**
-	 * Initialize SBB component.
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * Gets a reference to the XMPP RA so that messages can be sent out.
-	 * 
+	 * @see javax.slee.Sbb#setSbbContext(javax.slee.SbbContext)
 	 */
 	public void setSbbContext(SbbContext context) {
 		this.sbbContext = context;
-		if (logger == null) {
-			logger = sbbContext.getTracer(getClass().getSimpleName());
-		}
+		this.tracer = sbbContext.getTracer(getClass().getSimpleName());
 		try {
-			logger.info("Called setSbbContext!");
-			
-			Context myEnv = (Context) new InitialContext().lookup("java:comp/env");
-			
+			Context myEnv = (Context) new InitialContext()
+					.lookup("java:comp/env");
+
 			xmppSbbInterface = (XmppResourceAdaptorSbbInterface) myEnv
 					.lookup("slee/resources/xmpp/2.0/xmppinterface");
-
+			xmppActivityContextInterfaceFactory = (XmppActivityContextInterfaceFactory) myEnv
+					.lookup("slee/resources/xmpp/2.0/factoryprovider");
 			// env-entries
 			username = (String) myEnv.lookup("username");
 			password = (String) myEnv.lookup("password");
 
-			logger.info("setSbbContext() Retrieved uid[" + username + "]," +
-					" passwd[" + password + "]");
+			tracer.info("setSbbContext() Retrieved uid[" + username + "],"
+					+ " passwd[" + password + "]");
 		} catch (NamingException ne) {
-			logger.severe("Could not set SBB context:" + ne.getMessage(),ne);
+			tracer.severe("Could not set SBB context:" + ne.getMessage(), ne);
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#unsetSbbContext()
+	 */
 	public void unsetSbbContext() {
 		this.sbbContext = null;
+		this.tracer = null;
+		xmppActivityContextInterfaceFactory = null;
+		xmppSbbInterface = null;
 	}
 
-	// TODO: Implement the lifecycle methods if required
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbCreate()
+	 */
 	public void sbbCreate() throws javax.slee.CreateException {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbPostCreate()
+	 */
 	public void sbbPostCreate() throws javax.slee.CreateException {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbActivate()
+	 */
 	public void sbbActivate() {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbPassivate()
+	 */
 	public void sbbPassivate() {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbRemove()
+	 */
 	public void sbbRemove() {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbLoad()
+	 */
 	public void sbbLoad() {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbStore()
+	 */
 	public void sbbStore() {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbExceptionThrown(java.lang.Exception,
+	 * java.lang.Object, javax.slee.ActivityContextInterface)
+	 */
 	public void sbbExceptionThrown(Exception exception, Object event,
 			ActivityContextInterface activity) {
 	}
 
-	public void sbbRolledBack(RolledBackContext context) {
-	}
-
-	/**
-	 * Convenience method to retrieve the SbbContext object stored in
-	 * setSbbContext.
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * TODO: If your SBB doesn't require the SbbContext object you may remove
-	 * this method, the sbbContext variable and the variable assignment in
-	 * setSbbContext().
-	 * 
-	 * @return this SBB's SbbContext object
+	 * @see javax.slee.Sbb#sbbRolledBack(javax.slee.RolledBackContext)
 	 */
-
-	protected SbbContext getSbbContext() {
-		return sbbContext;
-	}
-
-	protected String getTraceMessageType() {
-		return "GoogleTalkBotService";
+	public void sbbRolledBack(RolledBackContext context) {
 	}
 
 }
