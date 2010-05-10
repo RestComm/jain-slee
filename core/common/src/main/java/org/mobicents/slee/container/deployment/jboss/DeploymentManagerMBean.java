098cf4d2fca3b140757079770f58f8697b8b37e5
brainslog <brainslog@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-05-10 12:58:59 +0000

    Add JAIN SLEE persistent deploy/undeploy via JMX
    Fixes Issue #1394
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@11643 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- core/common/src/main/java/org/mobicents/slee/container/deployment/jboss/DeploymentManagerMBean.java
+++ core/common/src/main/java/org/mobicents/slee/container/deployment/jboss/DeploymentManagerMBean.java
@@ -1,7 +1,34 @@
 package org.mobicents.slee.container.deployment.jboss;
 
+import java.net.URL;
+
+import org.jboss.deployers.spi.DeploymentException;
+
 public interface DeploymentManagerMBean {
 
-	public String showStatus();
-	
+  /**
+   * Displays information regarding the JBoss JAIN SLEE DU Deployer, providing
+   * information regarding deployed DU's and others with missing dependencies.
+   * 
+   * @return a HTML string with the current status information
+   */
+  public String showStatus();
+
+  /**
+   * Deploys a DU from a remote or local location in a persistent way, ie,
+   * copying it to JBOSS_HOME/server/<node>/deploy folder.
+   * 
+   * @param deployableUnitURL the URL to the DU to be deployed
+   * @throws DeploymentException if it's not possible to deploy this DU
+   */
+  void persistentInstall(URL deployableUnitURL) throws DeploymentException;
+
+  /**
+   * Undeploys a persistently deployed DU by deleting it from the server deploy folder.
+   * 
+   * @param deployableUnitURL the URL to the DU to be undeployed
+   * @throws DeploymentException if it's not possible to undeploy this DU
+   */
+  void persistentUninstall(URL deployableUnitURL) throws DeploymentException;
+
 }
