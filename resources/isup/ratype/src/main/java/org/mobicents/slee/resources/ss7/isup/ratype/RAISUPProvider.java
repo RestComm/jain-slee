d787c72b8aedf541a95d8c1cc6a06bff3015417b
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-06-08 17:58:22 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@12132 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- resources/isup/ratype/src/main/java/org/mobicents/slee/resources/ss7/isup/ratype/RAISUPProvider.java
+++ resources/isup/ratype/src/main/java/org/mobicents/slee/resources/ss7/isup/ratype/RAISUPProvider.java
@@ -16,20 +16,58 @@ import org.mobicents.protocols.ss7.isup.TransactionAlredyExistsException;
 import org.mobicents.protocols.ss7.isup.message.ISUPMessage;
 
 public interface RAISUPProvider {
-
+	
+	/**
+	 * Create client transaction activity
+	 * @param arg0
+	 * @return
+	 * @throws TransactionAlredyExistsException
+	 * @throws IllegalArgumentException
+	 * @throws ActivityAlreadyExistsException
+	 * @throws NullPointerException
+	 * @throws IllegalStateException
+	 * @throws SLEEException
+	 * @throws StartActivityException
+	 */
 	public ISUPClientTransaction createClientTransaction(ISUPMessage arg0) throws TransactionAlredyExistsException,
 			IllegalArgumentException, ActivityAlreadyExistsException, NullPointerException, IllegalStateException, SLEEException,
 			StartActivityException;
-
+	/**
+	 * Create server transaction activity
+	 * @param arg0
+	 * @return
+	 * @throws TransactionAlredyExistsException
+	 * @throws IllegalArgumentException
+	 * @throws ActivityAlreadyExistsException
+	 * @throws NullPointerException
+	 * @throws IllegalStateException
+	 * @throws SLEEException
+	 * @throws StartActivityException
+	 */
 	public ISUPServerTransaction createServerTransaction(ISUPMessage arg0) throws TransactionAlredyExistsException,
 			IllegalArgumentException, ActivityAlreadyExistsException, NullPointerException, IllegalStateException, SLEEException,
 			StartActivityException;
 
+	/**
+	 * Get message factory.
+	 * @return
+	 */
 	public ISUPMessageFactory getMessageFactory();
-
+	/**
+	 * Get parameter factory.
+	 * @return
+	 */
 	public ISUPParameterFactory getParameterFactory();
-
+	/**
+	 * Send message statelesly.
+	 * @param arg0
+	 * @throws ParameterRangeInvalidException
+	 * @throws IOException
+	 */
 	public void sendMessage(ISUPMessage arg0) throws ParameterRangeInvalidException, IOException;
-	
+	/**
+	 * Determine if transport layer is connected and links are up.
+	 * @return
+	 */
 	public boolean isTransportUp();
 }
