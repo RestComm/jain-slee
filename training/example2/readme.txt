b76081ad8a37d34d9d501f2891627818818a4620
amit.bhayani <amit.bhayani@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-07-31 14:12:17 +0000

    committing training example 2
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@13359 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ training/example2/readme.txt
@@ -0,0 +1,10 @@
+Before any example is deployed make sure that LabRA is deployed. 
+
+
+Training Example2
+In this example we will see how child entity is created
+How CMP works
+How environment entry can be used
+How javax.slee.SbbLocalObject is implemented and used
+
+
---
+++ training/example2/sbb/src/main/java/org/mobicents/slee/training/example2/ChildSbbLocalObject.java
@@ -0,0 +1,21 @@
+/*
+ * ***************************************************
+ *                                                 *
+ *  Mobicents: The Open Source JSLEE Platform      *
+ *                                                 *
+ *  Distributable under LGPL license.              *
+ *  See terms of license at gnu.org.               *
+ *                                                 *
+ ***************************************************
+ */
+package org.mobicents.slee.training.example2;
+
+/**
+ * Methods declared in ChildSbbLocalObject can be called synchronously  
+ * @author amit bhayani
+ *
+ */
+public interface ChildSbbLocalObject extends javax.slee.SbbLocalObject {
+	public void synchronousCall();
+
+}
