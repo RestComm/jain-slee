f96165c22c56fd2cf128a2d449f0a86f40c747be
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-09-05 11:52:01 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@13956 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- api/extensions/src/main/java/org/mobicents/slee/SbbContextExt.java
+++ api/extensions/src/main/java/org/mobicents/slee/SbbContextExt.java
@@ -1,5 +1,6 @@
 package org.mobicents.slee;
 
+import javax.slee.ActivityContextInterface;
 import javax.slee.Sbb;
 import javax.slee.SbbContext;
 import javax.slee.facilities.ActivityContextNamingFacility;
@@ -9,12 +10,14 @@ import javax.slee.nullactivity.NullActivityContextInterfaceFactory;
 import javax.slee.nullactivity.NullActivityFactory;
 import javax.slee.profile.ProfileFacility;
 import javax.slee.profile.ProfileTableActivityContextInterfaceFactory;
+import javax.slee.resource.ResourceAdaptorTypeID;
 import javax.slee.serviceactivity.ServiceActivityContextInterfaceFactory;
 import javax.slee.serviceactivity.ServiceActivityFactory;
 
 /**
- * Extension of {@link SbbContext}, which exposes facilities, allowing the
- * {@link Sbb} developer to avoid JNDI usage.
+ * Extension of {@link SbbContext}, which exposes facilities, factories and
+ * Resource Adaptor interfaces, allowing the {@link Sbb} developer to avoid JNDI
+ * usage.
  * 
  * @author martins
  * 
@@ -22,11 +25,20 @@ import javax.slee.serviceactivity.ServiceActivityFactory;
 public interface SbbContextExt extends SbbContext {
 
 	/**
-	 * Retrieves the Alarm Facility.
+	 * Retrieves the {@link ActivityContextInterface} factory for the Resource
+	 * Adaptor Type with the specified ID.
 	 * 
+	 * @param raTypeID
 	 * @return
+	 * @throws NullPointerException
+	 *             if the raTypeID parameter is null
+	 * @throws IllegalArgumentException
+	 *             if there is no reference to such factory in the Sbb's XML
+	 *             descriptor.
 	 */
-	public AlarmFacility getAlarmFacility();
+	public Object getActivityContextInterfaceFactory(
+			ResourceAdaptorTypeID raTypeID) throws NullPointerException,
+			IllegalArgumentException;
 
 	/**
 	 * Retrieves the Activity Context Naming Facility.
@@ -34,6 +46,13 @@ public interface SbbContextExt extends SbbContext {
 	 * @return
 	 */
 	public ActivityContextNamingFacility getActivityContextNamingFacility();
+
+	/**
+	 * Retrieves the Alarm Facility.
+	 * 
+	 * @return
+	 */
+	public AlarmFacility getAlarmFacility();
 
 	/**
 	 * Retrieves the Null Activity Context Interface Factory.
@@ -50,13 +69,6 @@ public interface SbbContextExt extends SbbContext {
 	public NullActivityFactory getNullActivityFactory();
 
 	/**
-	 * Retrieves the Timer Facility.
-	 * 
-	 * @return
-	 */
-	public TimerFacility getTimerFacility();
-
-	/**
 	 * Retrieves the Profile Facility.
 	 * 
 	 * @return
@@ -71,6 +83,24 @@ public interface SbbContextExt extends SbbContext {
 	public ProfileTableActivityContextInterfaceFactory getProfileTableActivityContextInterfaceFactory();
 
 	/**
+	 * Retrieves the interface to interact with a specific Resource Adaptor
+	 * entity, identified by both the entity link name and the Resource Adaptor
+	 * Type ID.
+	 * 
+	 * @param raTypeID
+	 * @param raEntityLink
+	 * @return
+	 * @throws NullPointerException
+	 *             if any of parameters is null
+	 * @throws IllegalArgumentException
+	 *             if there is no reference to such RA Sbb interface in the
+	 *             Sbb's XML descriptor.
+	 */
+	public Object getResourceAdaptorInterface(ResourceAdaptorTypeID raTypeID,
+			String raEntityLink) throws NullPointerException,
+			IllegalArgumentException;
+
+	/**
 	 * Retrieves the Service Activity Context Interface Factory.
 	 * 
 	 * @return
@@ -83,4 +113,11 @@ public interface SbbContextExt extends SbbContext {
 	 * @return
 	 */
 	public ServiceActivityFactory getServiceActivityFactory();
+
+	/**
+	 * Retrieves the Timer Facility.
+	 * 
+	 * @return
+	 */
+	public TimerFacility getTimerFacility();
 }
