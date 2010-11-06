fa0800110e18b1d4e1f673a576d2ba265d9ad93e
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-11-06 08:07:28 +0000

    Adding XDM Client SLEE Enabler Docs
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@15079 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- enablers/xdm-client/sbb/src/main/java/org/mobicents/slee/enabler/xdmc/XDMClientParent.java
+++ enablers/xdm-client/sbb/src/main/java/org/mobicents/slee/enabler/xdmc/XDMClientParent.java
@@ -7,12 +7,12 @@ import java.net.URI;
 
 /**
  * @author martins
- *
+ * 
  */
 public interface XDMClientParent {
 
 	/**
-	 * Provides response of a get request.
+	 * Provides the response for an XML resource GET request.
 	 * 
 	 * @param uri
 	 * @param responseCode
@@ -24,46 +24,48 @@ public interface XDMClientParent {
 			String content, String eTag);
 
 	/**
-	 * Provides response of a put request.
+	 * Provides the response for an XML resource PUT request.
 	 * 
 	 * @param uri
 	 * @param responseCode
 	 * @param eTag
-	 * @param responseContent 
+	 * @param responseContent
 	 */
-	public void putResponse(URI uri, int responseCode, String responseContent, String eTag);
+	public void putResponse(URI uri, int responseCode, String responseContent,
+			String eTag);
 
 	/**
-	 * Provides response of a delete request.
+	 * Provides the response for an XML resource DELETE request.
 	 * 
 	 * @param uri
 	 * @param responseCode
 	 * @param eTag
-	 * @param responseContent 
+	 * @param responseContent
 	 */
-	public void deleteResponse(URI uri, int responseCode, String responseContent, String eTag);
+	public void deleteResponse(URI uri, int responseCode,
+			String responseContent, String eTag);
 
 	/**
 	 * Callback that indicates a document subscribed in the XDM client was
 	 * updated
 	 */
-	//public void documentUpdated(DocumentSelector documentSelector,
-	//		String oldETag, String newETag, String documentAsString);
+	// public void documentUpdated(DocumentSelector documentSelector,
+	// String oldETag, String newETag, String documentAsString);
 
 	/**
 	 * Callback that indicates a element in a subscribed document was updated
 	 */
-	//public void elementUpdated(DocumentSelector documentSelector,
-	//		NodeSelector nodeSelector, Map<String, String> namespaces,
-	//		String oldETag, String newETag, String documentAsString,
-	//		String elementAsString);
+	// public void elementUpdated(DocumentSelector documentSelector,
+	// NodeSelector nodeSelector, Map<String, String> namespaces,
+	// String oldETag, String newETag, String documentAsString,
+	// String elementAsString);
 
 	/**
 	 * Callback that indicates a attribute in a subscribed document was updated
 	 */
-	//public void attributeUpdated(DocumentSelector documentSelector,
-	//		NodeSelector nodeSelector, AttributeSelector attributeSelector,
-	//		Map<String, String> namespaces, String oldETag, String newETag,
-	//		String documentAsString, String attributeValue);
-	
+	// public void attributeUpdated(DocumentSelector documentSelector,
+	// NodeSelector nodeSelector, AttributeSelector attributeSelector,
+	// Map<String, String> namespaces, String oldETag, String newETag,
+	// String documentAsString, String attributeValue);
+
 }
