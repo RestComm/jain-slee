a391bc588e0030cc41ed4843bd95e8424b829c06
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-01-13 10:07:59 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@16399 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- examples/sip-uas/sipp/run-ha.sh
+++ examples/sip-uas/sipp/run-ha.sh
@@ -1,1 +1,1 @@
-sipp 127.0.0.1:5060 -nd -trace_err -sf uac.xml -i 127.0.0.1 -p 5050 -r 1 -m 10000000 -l 10000000
+sipp 127.0.0.1:5060 -inf users.csv -nd -trace_err -sf uac.xml -i 127.0.0.1 -p 5050 -r 1 -m 10000000 -l 10000000
