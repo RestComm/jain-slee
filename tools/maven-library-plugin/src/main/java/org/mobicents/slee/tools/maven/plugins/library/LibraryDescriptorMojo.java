def53e7fdf22c89df1ef021f00231af9a6f1df85
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-02-01 15:59:41 +0000

    Update Issue 2318. Exposes copy-dependencies goal of maven-dependency-plugin
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@16755 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- tools/maven-library-plugin/src/main/java/org/mobicents/slee/tools/maven/plugins/library/LibraryDescriptorMojo.java
+++ tools/maven-library-plugin/src/main/java/org/mobicents/slee/tools/maven/plugins/library/LibraryDescriptorMojo.java
@@ -20,305 +20,302 @@ import org.mobicents.slee.tools.maven.plugins.library.pojos.LibraryRef;
 /**
  * Base class for creating a library jar.
  * 
- * @goal create
- * @phase generate-resources
- * 
  * @author <a href="brainslog@gmail.com"> Alexandre Mendonca </a>
  * @author martins
  */
 public class LibraryDescriptorMojo extends AbstractMojo {
 
-  private LibraryRef libraryId;
+	private LibraryRef libraryId;
 
-  private List<LibraryRef> libraryRefs = new ArrayList<LibraryRef>();
+	private List<LibraryRef> libraryRefs = new ArrayList<LibraryRef>();
 
-  private String libraryRefsInConfig = null;
-  private String eventTypeRefs = null;
-  private String profileSpecRefs = null;
-  private String raTypeRefs = null;
-  private String sbbRefs = null;
+	private String libraryRefsInConfig = null;
+	private String eventTypeRefs = null;
+	private String profileSpecRefs = null;
+	private String raTypeRefs = null;
+	private String sbbRefs = null;
 
-  /**
-   * The Maven project.
-   * 
-   * @parameter expression="${project}"
-   * @required
-   * @readonly
-   */
-  private MavenProject project;
+	private MavenProject project;
 
-  /**
+	/**
 	 * Directory to be used as the source for SLEE component jars.
 	 * 
-	 * @parameter expression="${jarInputDirectory}" default-value="${project.build.outputDirectory}/jars"
 	 */
 	private File jarInputDirectory;
-	
-  /*
-   * (non-Javadoc)
-   * @see org.apache.maven.plugin.AbstractMojo#execute()
-   */
-  public void execute() throws MojoExecutionException
-  {
-    if(getLog().isDebugEnabled())
-    {
-      getLog().debug("Executing Maven JAIN SLEE 1.1 Library Plugin");
-    }
-    
-    // Obtain the name#vendor#version from pom
-    this.libraryId = getLibraryRef( project );
 
-    this.libraryRefsInConfig = getRefs( project, "library" );    
-    getLog().info("Library Refs Configuration:\n"+libraryRefsInConfig);
-    
-    this.eventTypeRefs = getRefs( project, "event-type" );    
-    getLog().info("EventType Refs:\n"+eventTypeRefs);  
-    
-    this.profileSpecRefs = getRefs( project , "profile-spec");
-    getLog().info("ProfileSpec Refs:\n"+profileSpecRefs);
-    
-    this.raTypeRefs = getRefs( project , "resource-adaptor-type");
-    getLog().info("RA Type Refs:\n"+raTypeRefs);
-    
-    this.sbbRefs = getRefs( project , "sbb");
-    getLog().info("SBB Refs:\n"+sbbRefs);
-    
-    if( this.libraryId == null )
-    {
-      throw new MojoExecutionException("Unable to get Library ID from pom, please verify.");
-    }
-    else
-    {
-      if(getLog().isDebugEnabled())
-      {
-        getLog().info( libraryId.getName() + "#" + libraryId.getVendor() + "#" + libraryId.getVersion() );
-      }
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.maven.plugin.AbstractMojo#execute()
+	 */
+	public void execute() throws MojoExecutionException {
+		if (getLog().isDebugEnabled()) {
+			getLog().debug("Executing Maven JAIN SLEE 1.1 Library Plugin");
+		}
 
-    // collect jar files 
-    Set<String> jars = collectFiles(jarInputDirectory,".jar");
-    
-    generateLibraryDeploymentDescritptor(jars);
+		// Obtain the name#vendor#version from pom
+		this.libraryId = getLibraryRef(project);
 
-  }
+		this.libraryRefsInConfig = getRefs(project, "library");
+		getLog().info("Library Refs Configuration:\n" + libraryRefsInConfig);
 
-  /**
-   * Generates the deployment descriptor for the library jar, based on the library-ref
-   * element, library jars and jars present at resources folder.
-   * 
-   * @throws MojoExecutionException
-   */
-  private void generateLibraryDeploymentDescritptor(Set<String> jars) throws MojoExecutionException
-  {
-    File libraryDescriptorDir = new File(project.getBuild().getOutputDirectory(), "META-INF");
+		this.eventTypeRefs = getRefs(project, "event-type");
+		getLog().info("EventType Refs:\n" + eventTypeRefs);
 
-    if (!libraryDescriptorDir.exists())
-    {
-      libraryDescriptorDir.mkdirs();
-    }
+		this.profileSpecRefs = getRefs(project, "profile-spec");
+		getLog().info("ProfileSpec Refs:\n" + profileSpecRefs);
 
-    File libraryDD = new File(libraryDescriptorDir.getAbsolutePath(), "library-jar.xml");
-    
-    String xml = null;
-    if (eventTypeRefs != null || profileSpecRefs != null || raTypeRefs != null || sbbRefs != null) {
-    	// extended
-    	xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" +
-        "<!DOCTYPE library-jar PUBLIC \r\n" +
-        "\t\t\"-//Sun Microsystems, Inc.//DTD JAIN SLEE Ext Library 1.1//EN\" \r\n" +
-        "\t\t\"http://mobicents.org/slee/dtd/slee-library-jar_1_1-ext.dtd\">\r\n\r\n" +
-        "<library-jar>\r\n\t<library>\r\n";
-    }
-    else {
-    	// standard is enough
-    	xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" +
-        "<!DOCTYPE library-jar PUBLIC \r\n" +
-        "\t\"-//Sun Microsystems, Inc.//DTD JAIN SLEE Library 1.1//EN\" \r\n" +
-        "\t\"http://java.sun.com/dtd/slee-library-jar_1_1.dtd\">\r\n" +
-        "<library-jar>\r\n" +
-        "\t<library>\r\n";
-    }
-    
-    xml += this.libraryId.toXmlEntry();
+		this.raTypeRefs = getRefs(project, "resource-adaptor-type");
+		getLog().info("RA Type Refs:\n" + raTypeRefs);
 
-    if (eventTypeRefs != null) {
-    	xml += eventTypeRefs + "\r\n";
-    }
-    
-    if (libraryRefsInConfig != null) {
-    	xml += libraryRefsInConfig + "\r\n";
-    }
-    
-    if (libraryRefs != null && !libraryRefs.isEmpty()) {
-    	for(LibraryRef libraryRef : this.libraryRefs) {
-    		xml += libraryRef.toXmlEntryWithRef();
-    	}
-    	xml += "\r\n";
-    }
-    
-    if (profileSpecRefs != null) {
-    	xml += profileSpecRefs + "\r\n";
-    }
-    
-    if (raTypeRefs != null) {
-    	xml += raTypeRefs + "\r\n";
-    }
+		this.sbbRefs = getRefs(project, "sbb");
+		getLog().info("SBB Refs:\n" + sbbRefs);
 
-    if (sbbRefs != null) {
-    	xml += sbbRefs + "\r\n";
-    }
-    
-    for(String jar : jars)
-    {
-      xml += "\t\t<jar>\r\n" +
-      		"\t\t\t<jar-name>"+jar+"</jar-name>\r\n" +
-      				"\t\t<jar>\r\n";
-    }
+		if (this.libraryId == null) {
+			throw new MojoExecutionException(
+					"Unable to get Library ID from pom, please verify.");
+		} else {
+			if (getLog().isDebugEnabled()) {
+				getLog().info(
+						libraryId.getName() + "#" + libraryId.getVendor() + "#"
+								+ libraryId.getVersion());
+			}
+		}
 
-    xml += "\t</library>\r\n</library-jar>\r\n";
+		// collect jar files
+		Set<String> jars = collectFiles(jarInputDirectory, ".jar");
 
-    getLog().info("Generated Library descriptor: " + libraryDD.getAbsolutePath() + "\n" + xml + "\n");
+		generateLibraryDeploymentDescritptor(jars);
 
-    try
-    {
-      BufferedWriter out = new BufferedWriter(new FileWriter(libraryDD));
-      out.write(xml);
-      out.close();
-    }
-    catch ( IOException e )
-    {
-      getLog().error( "Failed to create deployment descriptor in " + libraryDD.getAbsolutePath(), e );
-    }
+	}
 
-  }
+	/**
+	 * Generates the deployment descriptor for the library jar, based on the
+	 * library-ref element, library jars and jars present at resources folder.
+	 * 
+	 * @throws MojoExecutionException
+	 */
+	private void generateLibraryDeploymentDescritptor(Set<String> jars)
+			throws MojoExecutionException {
+		File libraryDescriptorDir = new File(project.getBuild()
+				.getOutputDirectory(), "META-INF");
 
-  /**
-   * Obtains the library-name, library-vendor, library-version into a LibraryRef element. 
-   * 
-   * @param mavenProject the maven project to obtain it from
-   * @return
-   */
+		if (!libraryDescriptorDir.exists()) {
+			libraryDescriptorDir.mkdirs();
+		}
 
-  private LibraryRef getLibraryRef(MavenProject mavenProject)
-  {
-    String libraryName;
-    String libraryVendor;
-    String libraryVersion;
+		File libraryDD = new File(libraryDescriptorDir.getAbsolutePath(),
+				"library-jar.xml");
 
-    String libraryDescription = null;
+		String xml = null;
+		if (eventTypeRefs != null || profileSpecRefs != null
+				|| raTypeRefs != null || sbbRefs != null) {
+			// extended
+			xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+					+ "<!DOCTYPE library-jar PUBLIC \r\n"
+					+ "\t\t\"-//Sun Microsystems, Inc.//DTD JAIN SLEE Ext Library 1.1//EN\" \r\n"
+					+ "\t\t\"http://mobicents.org/slee/dtd/slee-library-jar_1_1-ext.dtd\">\r\n\r\n"
+					+ "<library-jar>\r\n\t<library>\r\n";
+		} else {
+			// standard is enough
+			xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+					+ "<!DOCTYPE library-jar PUBLIC \r\n"
+					+ "\t\"-//Sun Microsystems, Inc.//DTD JAIN SLEE Library 1.1//EN\" \r\n"
+					+ "\t\"http://java.sun.com/dtd/slee-library-jar_1_1.dtd\">\r\n"
+					+ "<library-jar>\r\n" + "\t<library>\r\n";
+		}
 
-    for(Object pObject : mavenProject.getBuildPlugins())
-    {
-      Plugin plugin = (Plugin)pObject;
-      if( plugin.getArtifactId().equals( "maven-library-plugin" ) )
-      {
-        Xpp3Dom configuration = (Xpp3Dom)plugin.getConfiguration();
+		xml += this.libraryId.toXmlEntry();
 
-        if(configuration != null)
-        {
-          if(configuration.getChildren("library-name").length > 0)
-          {
-            libraryName = configuration.getChildren("library-name")[0].getValue();
-          }
-          else
-          {
-            getLog().error( "Library Name missing in plugin configuration!" );
-            return null;
-          }
+		if (eventTypeRefs != null) {
+			xml += eventTypeRefs + "\r\n";
+		}
 
-          if(configuration.getChildren("library-vendor").length > 0)
-          {
-            libraryVendor = configuration.getChildren("library-vendor")[0].getValue();
-          }
-          else
-          {
-            getLog().error( "Library Vendor missing in plugin configuration!" );
-            return null;
-          }
+		if (libraryRefsInConfig != null) {
+			xml += libraryRefsInConfig + "\r\n";
+		}
 
-          if(configuration.getChildren("library-version").length > 0)
-          {
-            libraryVersion = configuration.getChildren("library-version")[0].getValue();
-          }
-          else
-          {
-            getLog().error( "Library Version missing in plugin configuration!" );
-            return null;
-          }
+		if (libraryRefs != null && !libraryRefs.isEmpty()) {
+			for (LibraryRef libraryRef : this.libraryRefs) {
+				xml += libraryRef.toXmlEntryWithRef();
+			}
+			xml += "\r\n";
+		}
 
-          if(configuration.getChildren("description").length > 0)
-          {
-            libraryDescription = configuration.getChildren("description")[0].getValue();
-          }
-        }
-        else
-        {
-          getLog().error( "Configuration missing in plugin!" );
-          return null;         
-        }
+		if (profileSpecRefs != null) {
+			xml += profileSpecRefs + "\r\n";
+		}
 
-        return new LibraryRef(libraryName, libraryVendor, libraryVersion, libraryDescription);    
-      }
-    }
+		if (raTypeRefs != null) {
+			xml += raTypeRefs + "\r\n";
+		}
 
-    return null;
-  }
-  
-  private String getRefs(MavenProject mavenProject, String refName)  {
+		if (sbbRefs != null) {
+			xml += sbbRefs + "\r\n";
+		}
 
-	  for(Object pObject : mavenProject.getBuildPlugins()) {
+		for (String jar : jars) {
+			xml += "\t\t<jar>\r\n" + "\t\t\t<jar-name>" + jar
+					+ "</jar-name>\r\n" + "\t\t<jar>\r\n";
+		}
 
-		  Plugin plugin = (Plugin)pObject;
+		xml += "\t</library>\r\n</library-jar>\r\n";
 
-		  if(!plugin.getArtifactId().equals( "maven-library-plugin" )) {
-			  continue;
-		  }
+		getLog().info(
+				"Generated Library descriptor: " + libraryDD.getAbsolutePath()
+						+ "\n" + xml + "\n");
 
-		  Xpp3Dom configuration = (Xpp3Dom)plugin.getConfiguration();
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(libraryDD));
+			out.write(xml);
+			out.close();
+		} catch (IOException e) {
+			getLog().error(
+					"Failed to create deployment descriptor in "
+							+ libraryDD.getAbsolutePath(), e);
+		}
 
-		  if(configuration != null) {
-			  String result = null;
-			  for (Xpp3Dom child : configuration.getChildren(refName+"-ref")) {
-				  String childXml = "\t\t<"+refName+"-ref>";
-				  try { 
-					  childXml += "\n\t\t\t<"+refName+"-name>" + child.getChild(refName+"-name").getValue() + "</"+refName+"-name>";
-					  childXml += "\n\t\t\t<"+refName+"-vendor>" + child.getChild(refName+"-vendor").getValue() + "</"+refName+"-vendor>";
-					  childXml += "\n\t\t\t<"+refName+"-version>" + child.getChild(refName+"-version").getValue() + "</"+refName+"-version>";
-					  childXml += "\n\t\t</"+refName+"-ref>";
-				  }
-				  catch (Exception e) {
-					getLog().error("Failed to load ref of type "+refName,e);
-					throw new RuntimeException(e);
-				  }
-				  if (result == null) {
-					result = childXml; 
-				 }
-				 else {
-					 result += "\r\n" + childXml;
-				 }
-			  }
-			  return result;		  }
-		  else {
-			  getLog().info( "Configuration missing in plugin!" );
-			  return null;         
-		  }      
-	  }
-    
-	  return null;
-  }
+	}
 
-  private Set<String> collectFiles(File inputDirectory, String suffix) {
+	/**
+	 * Obtains the library-name, library-vendor, library-version into a
+	 * LibraryRef element.
+	 * 
+	 * @param mavenProject
+	 *            the maven project to obtain it from
+	 * @return
+	 */
+
+	private LibraryRef getLibraryRef(MavenProject mavenProject) {
+		String libraryName;
+		String libraryVendor;
+		String libraryVersion;
+
+		String libraryDescription = null;
+
+		for (Object pObject : mavenProject.getBuildPlugins()) {
+			Plugin plugin = (Plugin) pObject;
+			if (plugin.getArtifactId().equals("maven-library-plugin")) {
+				Xpp3Dom configuration = (Xpp3Dom) plugin.getConfiguration();
+
+				if (configuration != null) {
+					if (configuration.getChildren("library-name").length > 0) {
+						libraryName = configuration.getChildren("library-name")[0]
+								.getValue();
+					} else {
+						getLog().error(
+								"Library Name missing in plugin configuration!");
+						return null;
+					}
+
+					if (configuration.getChildren("library-vendor").length > 0) {
+						libraryVendor = configuration
+								.getChildren("library-vendor")[0].getValue();
+					} else {
+						getLog().error(
+								"Library Vendor missing in plugin configuration!");
+						return null;
+					}
+
+					if (configuration.getChildren("library-version").length > 0) {
+						libraryVersion = configuration
+								.getChildren("library-version")[0].getValue();
+					} else {
+						getLog().error(
+								"Library Version missing in plugin configuration!");
+						return null;
+					}
+
+					if (configuration.getChildren("description").length > 0) {
+						libraryDescription = configuration
+								.getChildren("description")[0].getValue();
+					}
+				} else {
+					getLog().error("Configuration missing in plugin!");
+					return null;
+				}
+
+				return new LibraryRef(libraryName, libraryVendor,
+						libraryVersion, libraryDescription);
+			}
+		}
+
+		return null;
+	}
+
+	private String getRefs(MavenProject mavenProject, String refName) {
+
+		for (Object pObject : mavenProject.getBuildPlugins()) {
+
+			Plugin plugin = (Plugin) pObject;
+
+			if (!plugin.getArtifactId().equals("maven-library-plugin")) {
+				continue;
+			}
+
+			Xpp3Dom configuration = (Xpp3Dom) plugin.getConfiguration();
+
+			if (configuration != null) {
+				String result = null;
+				for (Xpp3Dom child : configuration
+						.getChildren(refName + "-ref")) {
+					String childXml = "\t\t<" + refName + "-ref>";
+					try {
+						childXml += "\n\t\t\t<" + refName + "-name>"
+								+ child.getChild(refName + "-name").getValue()
+								+ "</" + refName + "-name>";
+						childXml += "\n\t\t\t<"
+								+ refName
+								+ "-vendor>"
+								+ child.getChild(refName + "-vendor")
+										.getValue() + "</" + refName
+								+ "-vendor>";
+						childXml += "\n\t\t\t<"
+								+ refName
+								+ "-version>"
+								+ child.getChild(refName + "-version")
+										.getValue() + "</" + refName
+								+ "-version>";
+						childXml += "\n\t\t</" + refName + "-ref>";
+					} catch (Exception e) {
+						getLog().error("Failed to load ref of type " + refName,
+								e);
+						throw new RuntimeException(e);
+					}
+					if (result == null) {
+						result = childXml;
+					} else {
+						result += "\r\n" + childXml;
+					}
+				}
+				return result;
+			} else {
+				getLog().info("Configuration missing in plugin!");
+				return null;
+			}
+		}
+
+		return null;
+	}
+
+	private Set<String> collectFiles(File inputDirectory, String suffix) {
 
 		if (getLog().isDebugEnabled()) {
-			getLog().debug("Collecting non hidden files with "+suffix+" name sufix from directory "+inputDirectory.getAbsolutePath());
+			getLog().debug(
+					"Collecting non hidden files with " + suffix
+							+ " name sufix from directory "
+							+ inputDirectory.getAbsolutePath());
 		}
-		
+
 		if (inputDirectory == null || !inputDirectory.exists()
 				|| !inputDirectory.isDirectory()) {
 			return Collections.emptySet();
 		}
 
 		if (getLog().isDebugEnabled()) {
-			getLog().debug("Directory "+inputDirectory.getAbsolutePath()+" sucessfully validated.");
+			getLog().debug(
+					"Directory " + inputDirectory.getAbsolutePath()
+							+ " sucessfully validated.");
 		}
 
 		Set<String> result = new HashSet<String>();
