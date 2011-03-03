7f62f1d71661cafe75ee85e5ef72e7d2b1587685
brainslog <brainslog@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-03-03 01:36:50 +0000

    Allows EclipSLEE to have default module names
    Update Issue 2398
    Status: Fixed
    When adding a module, if the name is blank than the default name will be used. An info message indicating such behavior is also present.
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@17122 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- tools/eclipslee/plugins/org.mobicents.eclipslee.servicecreation/src/org/mobicents/eclipslee/servicecreation/popup/actions/AddModuleAction.java
+++ tools/eclipslee/plugins/org.mobicents.eclipslee.servicecreation/src/org/mobicents/eclipslee/servicecreation/popup/actions/AddModuleAction.java
@@ -32,7 +32,9 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 import org.mobicents.eclipslee.util.maven.MavenProjectUtils;
 
 /**
@@ -151,100 +153,96 @@ public class AddModuleAction implements IObjectActionDelegate {
       boolean moduleExists = true;
       try {
         String moduleName = moduleNamePage.getModuleName();
-        if(!moduleName.equals("")) {
-          String fullModuleName = moduleName + "-" + moduleType;
-          IFolder moduleFolder = project.getFolder(fullModuleName);
-          moduleExists = moduleFolder.exists();
-          if(!moduleExists) {
-            // Create maven folder structure
-            moduleFolder.create(true, true, null);
-            moduleFolder.getFolder("src").create(true, true, null);
-            moduleFolder.getFolder("src/main").create(true, true, null);
-            moduleFolder.getFolder("src/main/java").create(true, true, null);
-            moduleFolder.getFolder("src/main/resources").create(true, true, null);
+        boolean isDefaultModule = moduleName.equals("");
+        String fullModuleName = moduleName + (isDefaultModule ? "" : "-") + moduleType;
+        IFolder moduleFolder = project.getFolder(fullModuleName);
+        moduleExists = moduleFolder.exists();
+        if(!moduleExists) {
+          // Create maven folder structure
+          moduleFolder.create(true, true, null);
+          moduleFolder.getFolder("src").create(true, true, null);
+          moduleFolder.getFolder("src/main").create(true, true, null);
+          moduleFolder.getFolder("src/main/java").create(true, true, null);
+          moduleFolder.getFolder("src/main/resources").create(true, true, null);
 
-            // Generate pom.xml
-            if(moduleType.equals("events")) {
-              MavenProjectUtils.generateEventsModulePomFile(project, null, moduleName);
-            }
-            else if (moduleType.equals("sbb")) {
-              MavenProjectUtils.generateSBBModulePomFile(project, null, moduleName);
-            }
-            else if (moduleType.equals("profile-spec")) {
-              MavenProjectUtils.generateProfileSpecModulePomFile(project, null, moduleName);
-            }
-            else if (moduleType.equals("ratype")) {
-              MavenProjectUtils.generateRATypeModulePomFile(project, null, moduleName);
-            }
-            else if (moduleType.equals("ra")) {
-              MavenProjectUtils.generateRAModulePomFile(project, null, moduleName);
-            }
-            else if (moduleType.equals("library")) {
-              MavenProjectUtils.generateLibraryModulePomFile(project, null, moduleName);
-            }
-
-            project.refreshLocal(IResource.DEPTH_INFINITE, null);
-
-            // Update parent pom
-            MavenXpp3Reader reader = new MavenXpp3Reader();
-            Model parentModel = reader.read(new InputStreamReader(parentPom.getContents()));
-            // We'll add it in the end.. just to make sure it's last
-            parentModel.removeModule("du");
-            parentModel.addModule(fullModuleName);
-            parentModel.addModule("du");
-            MavenProjectUtils.writePomFile(parentModel, parentPom.getLocation().toOSString());
-
-            // Update deps poms
-            for(Button button : moduleNamePage.getDependants()) {
-              if(button.getSelection()) {
-                IFile dependantPom = project.getFolder((String) button.getData()).getFile("pom.xml");
-                Model dependantModel = reader.read(new InputStreamReader(dependantPom.getContents()));
-                Dependency dep = new Dependency();
-                dep.setGroupId(parentModel.getGroupId());
-                dep.setArtifactId(parentModel.getArtifactId() + "-" + fullModuleName);
-                dep.setVersion(parentModel.getVersion());
-                dependantModel.addDependency(dep);
-                MavenProjectUtils.writePomFile(dependantModel, dependantPom.getLocation().toOSString());
-              }
-            }
-
-            project.refreshLocal(IResource.DEPTH_INFINITE, null);
-
-            for(Button button : moduleNamePage.getDependencies()) {
-              if(button.getSelection()) {
-                IFile ownPom = project.getFolder(fullModuleName).getFile("pom.xml");
-                Model ownModel = reader.read(new InputStreamReader(ownPom.getContents()));
-                Dependency dep = new Dependency();
-                dep.setGroupId(parentModel.getGroupId());
-                dep.setArtifactId((String) button.getData());
-                dep.setVersion(parentModel.getVersion());
-                ownModel.addDependency(dep);
-                MavenProjectUtils.writePomFile(ownModel, ownPom.getLocation().toOSString());
-              }
-            }
-
-            project.refreshLocal(IResource.DEPTH_INFINITE, null);
-
-            // Add to Classpath
-            IJavaProject javaProject = JavaCore.create(parentPom.getProject());
-            IClasspathEntry[] classpath = javaProject.getRawClasspath();
-            IClasspathEntry[] extendedCP = new IClasspathEntry[classpath.length+2];
-            extendedCP[extendedCP.length-2] = JavaCore.newSourceEntry(project.getFullPath().append(fullModuleName + "/src/main/java"));
-            extendedCP[extendedCP.length-1] = JavaCore.newSourceEntry(project.getFullPath().append(fullModuleName + "/src/main/resources"));
-
-            // Copy contents from the first array to the extended array
-            System.arraycopy(classpath, 0, extendedCP, 0, classpath.length);
-
-            javaProject.setRawClasspath(extendedCP, null);
-
-            project.refreshLocal(IResource.DEPTH_INFINITE, null);
+          // Generate pom.xml
+          if(moduleType.equals("events")) {
+            MavenProjectUtils.generateEventsModulePomFile(project, null, isDefaultModule ? null : moduleName);
           }
-          else {
-            MessageDialog.openError(new Shell(), "Error Adding Module", "A module with the chosen name already exists. Please change the name.");
+          else if (moduleType.equals("sbb")) {
+            MavenProjectUtils.generateSBBModulePomFile(project, null, isDefaultModule ? null : moduleName);
           }
+          else if (moduleType.equals("profile-spec")) {
+            MavenProjectUtils.generateProfileSpecModulePomFile(project, null, isDefaultModule ? null : moduleName);
+          }
+          else if (moduleType.equals("ratype")) {
+            MavenProjectUtils.generateRATypeModulePomFile(project, null, isDefaultModule ? null : moduleName);
+          }
+          else if (moduleType.equals("ra")) {
+            MavenProjectUtils.generateRAModulePomFile(project, null, isDefaultModule ? null : moduleName);
+          }
+          else if (moduleType.equals("library")) {
+            MavenProjectUtils.generateLibraryModulePomFile(project, null, isDefaultModule ? null : moduleName);
+          }
+
+          project.refreshLocal(IResource.DEPTH_INFINITE, null);
+
+          // Update parent pom
+          MavenXpp3Reader reader = new MavenXpp3Reader();
+          Model parentModel = reader.read(new InputStreamReader(parentPom.getContents()));
+          // We'll add it in the end.. just to make sure it's last
+          parentModel.removeModule("du");
+          parentModel.addModule(fullModuleName);
+          parentModel.addModule("du");
+          MavenProjectUtils.writePomFile(parentModel, parentPom.getLocation().toOSString());
+
+          // Update deps poms
+          for(Button button : moduleNamePage.getDependants()) {
+            if(button.getSelection()) {
+              IFile dependantPom = project.getFolder((String) button.getData()).getFile("pom.xml");
+              Model dependantModel = reader.read(new InputStreamReader(dependantPom.getContents()));
+              Dependency dep = new Dependency();
+              dep.setGroupId(parentModel.getGroupId());
+              dep.setArtifactId(parentModel.getArtifactId() + "-" + fullModuleName);
+              dep.setVersion(parentModel.getVersion());
+              dependantModel.addDependency(dep);
+              MavenProjectUtils.writePomFile(dependantModel, dependantPom.getLocation().toOSString());
+            }
+          }
+
+          project.refreshLocal(IResource.DEPTH_INFINITE, null);
+
+          for(Button button : moduleNamePage.getDependencies()) {
+            if(button.getSelection()) {
+              IFile ownPom = project.getFolder(fullModuleName).getFile("pom.xml");
+              Model ownModel = reader.read(new InputStreamReader(ownPom.getContents()));
+              Dependency dep = new Dependency();
+              dep.setGroupId(parentModel.getGroupId());
+              dep.setArtifactId((String) button.getData());
+              dep.setVersion(parentModel.getVersion());
+              ownModel.addDependency(dep);
+              MavenProjectUtils.writePomFile(ownModel, ownPom.getLocation().toOSString());
+            }
+          }
+
+          project.refreshLocal(IResource.DEPTH_INFINITE, null);
+
+          // Add to Classpath
+          IJavaProject javaProject = JavaCore.create(parentPom.getProject());
+          IClasspathEntry[] classpath = javaProject.getRawClasspath();
+          IClasspathEntry[] extendedCP = new IClasspathEntry[classpath.length+2];
+          extendedCP[extendedCP.length-2] = JavaCore.newSourceEntry(project.getFullPath().append(fullModuleName + "/src/main/java"));
+          extendedCP[extendedCP.length-1] = JavaCore.newSourceEntry(project.getFullPath().append(fullModuleName + "/src/main/resources"));
+
+          // Copy contents from the first array to the extended array
+          System.arraycopy(classpath, 0, extendedCP, 0, classpath.length);
+
+          javaProject.setRawClasspath(extendedCP, null);
+
+          project.refreshLocal(IResource.DEPTH_INFINITE, null);
         }
         else {
-          MessageDialog.openError(new Shell(), "Error Adding Module", "The module name cannot be empty.");
+          MessageDialog.openError(new Shell(), "Error Adding Module", "A module with the chosen name already exists. Please change the name.");
         }
       }
       catch (Exception e) {
@@ -302,6 +300,15 @@ public class AddModuleAction implements IObjectActionDelegate {
             event.doit = false;
         }});
       
+      // Set the default name warning
+      new Label(composite, SWT.NONE); // just a dumb one, to fill the blank
+      Composite defNameWarnComp = new Composite(composite, SWT.NONE);
+      GridLayout defNameWarnGrid = new GridLayout();
+      defNameWarnGrid.numColumns = 2;
+      defNameWarnComp.setLayout(defNameWarnGrid);
+      new Label(defNameWarnComp, SWT.NONE).setImage(PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJS_INFO_TSK));
+      new Label(defNameWarnComp, SWT.NONE).setText("If a name is not provided for the module, \rthe default '" + moduleType +"' will be used.");
+
       try {
         MavenXpp3Reader reader = new MavenXpp3Reader();
         Model parentModel = reader.read(new InputStreamReader(parentPom.getContents()));
