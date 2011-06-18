0f86dbd2923d66352b6ca6a064b1b657ff01d265
brainslog <brainslog@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-06-18 00:50:19 +0000

    Removing initialization of confg properties in EclipSLEE RA generated code.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@18935 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- tools/eclipslee/plugin/plugins/org.mobicents.eclipslee.servicecreation/src/org/mobicents/eclipslee/servicecreation/wizards/ra/ResourceAdaptorWizard.java
+++ tools/eclipslee/plugin/plugins/org.mobicents.eclipslee.servicecreation/src/org/mobicents/eclipslee/servicecreation/wizards/ra/ResourceAdaptorWizard.java
@@ -1,24 +1,25 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2011, Red Hat, Inc. and/or its affiliates, and individual
- * contributors as indicated by the @authors tag. All rights reserved.
- * See the copyright.txt in the distribution for a full listing
- * of individual contributors.
- * 
- * This copyrighted material is made available to anyone wishing to use,
- * modify, copy, or redistribute it subject to the terms and conditions
- * of the GNU General Public License, v. 2.0.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License,
- * v. 2.0 along with this distribution; if not, write to the Free 
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
+ * Copyright 2011, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
 package org.mobicents.eclipslee.servicecreation.wizards.ra;
 
 import java.util.ArrayList;
@@ -164,6 +165,7 @@ public class ResourceAdaptorWizard extends BaseWizard {
         String configPropertyDefaultValue = (String) configProperty.get("Default Value");
         resourceAdaptor.addConfigProperty(configPropertyName, configPropertyType, configPropertyDefaultValue);
         // FIXME: Make this a method in ConfigPropertiesUtil...
+        /*
         String value = "null";
         if(configPropertyType.equals("java.lang.String")) {
           value = "\"" + configPropertyDefaultValue + "\"";
@@ -186,15 +188,16 @@ public class ResourceAdaptorWizard extends BaseWizard {
         else {
           value = configPropertyDefaultValue;
         }
+        */
         String simpleType = configPropertyType.replaceAll("java\\.lang\\.", "");
 
-        subConfigProperties += "\tprivate " + simpleType + " " + configPropertyName + " = " + value + ";\n";
+        subConfigProperties += "\tprivate " + simpleType + " " + configPropertyName /* + " = " + value  */ + ";\n";
 
         subRaConfigureMethod += "\n\t\tthis." + configPropertyName + " = (" + simpleType + ") properties.getProperty(\"" + configPropertyName + "\").getValue();\n";
-        subRaConfigureMethod += "\t\tif(this." + configPropertyName + ".equals(\"\")) {\n";
+        /*subRaConfigureMethod += "\t\tif(this." + configPropertyName + ".equals(\"\")) {\n";
         subRaConfigureMethod += "\t\t\t// TODO: define behavior when value is not present\n";
         subRaConfigureMethod += "\t\t\tthis." + configPropertyName + " = null;\n";
-        subRaConfigureMethod += "\t\t}\n";
+        subRaConfigureMethod += "\t\t}\n";*/
       }
 
       subs.put("__CONFIG_PROPERTIES__", subConfigProperties);
