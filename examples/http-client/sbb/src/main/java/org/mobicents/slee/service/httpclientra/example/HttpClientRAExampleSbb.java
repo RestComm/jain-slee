17f8d4891ec54b93fdfbaab56d8d109faba3b550
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-08-29 14:49:00 +0000

    Update Issue 2712.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@19940 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- examples/http-client/sbb/src/main/java/org/mobicents/slee/service/httpclientra/example/HttpClientRAExampleSbb.java
+++ examples/http-client/sbb/src/main/java/org/mobicents/slee/service/httpclientra/example/HttpClientRAExampleSbb.java
@@ -1,7 +1,5 @@
 package org.mobicents.slee.service.httpclientra.example;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
@@ -15,7 +13,6 @@ import javax.slee.RolledBackContext;
 import javax.slee.SbbContext;
 import javax.slee.facilities.TimerEvent;
 import javax.slee.facilities.TimerFacility;
-import javax.slee.facilities.TimerID;
 import javax.slee.facilities.TimerOptions;
 import javax.slee.facilities.TimerPreserveMissed;
 import javax.slee.facilities.Tracer;
@@ -23,28 +20,28 @@ import javax.slee.facilities.Tracer;
 import net.java.client.slee.resource.http.HttpClientActivity;
 import net.java.client.slee.resource.http.HttpClientActivityContextInterfaceFactory;
 import net.java.client.slee.resource.http.HttpClientResourceAdaptorSbbInterface;
-import net.java.client.slee.resource.http.HttpMethodName;
-import net.java.client.slee.resource.http.event.Response;
 import net.java.client.slee.resource.http.event.ResponseEvent;
 
-import org.apache.commons.httpclient.HttpMethod;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 
 import com.sun.syndication.feed.synd.SyndEntryImpl;
 import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 
 /**
- * This is a simple Service to demonstrate the usage of Http-client-ra 
- * This service reads the RSS Feed link from sbb-jar.xml and fetches the 
- * links periodically using ROME api (Look at https://rome.dev.java.net/ to know more about
- * ROME.) 
+ * This is a simple Service to demonstrate the usage of Http-client-ra This
+ * service reads the RSS Feed link from sbb-jar.xml and fetches the links
+ * periodically using ROME api (Look at https://rome.dev.java.net/ to know more
+ * about ROME.)
  * 
- * If there are changes in the RSS Feed, this service retrieves the content of the link
+ * If there are changes in the RSS Feed, this service retrieves the content of
+ * the link
  * 
  * @author amit.bhayani
- *
+ * 
  */
 public abstract class HttpClientRAExampleSbb implements javax.slee.Sbb {
 
@@ -58,97 +55,9 @@ public abstract class HttpClientRAExampleSbb implements javax.slee.Sbb {
 
 	private HttpClientResourceAdaptorSbbInterface raSbbInterface;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbActivate()
-	 */
-	public void sbbActivate() {
-		// TODO Auto-generated method stub
+	private long refreshTime;
 
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbCreate()
-	 */
-	public void sbbCreate() throws CreateException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbExceptionThrown(java.lang.Exception,
-	 *      java.lang.Object, javax.slee.ActivityContextInterface)
-	 */
-	public void sbbExceptionThrown(Exception arg0, Object arg1,
-			ActivityContextInterface arg2) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbLoad()
-	 */
-	public void sbbLoad() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbPassivate()
-	 */
-	public void sbbPassivate() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbPostCreate()
-	 */
-	public void sbbPostCreate() throws CreateException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbRemove()
-	 */
-	public void sbbRemove() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbRolledBack(javax.slee.RolledBackContext)
-	 */
-	public void sbbRolledBack(RolledBackContext arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#sbbStore()
-	 */
-	public void sbbStore() {
-		// TODO Auto-generated method stub
-
-	}
+	private String rssFeedString;
 
 	/*
 	 * (non-Javadoc)
@@ -158,8 +67,9 @@ public abstract class HttpClientRAExampleSbb implements javax.slee.Sbb {
 	public void setSbbContext(SbbContext context) {
 
 		this.sbbContext = context;
-		this.tracer = sbbContext.getTracer(HttpClientRAExampleSbb.class.getSimpleName());
-		
+		this.tracer = sbbContext.getTracer(HttpClientRAExampleSbb.class
+				.getSimpleName());
+
 		try {
 			Context ctx = (Context) new InitialContext()
 					.lookup("java:comp/env");
@@ -172,41 +82,38 @@ public abstract class HttpClientRAExampleSbb implements javax.slee.Sbb {
 
 			// Getting Timer Facility interface
 			timerFacility = (TimerFacility) ctx.lookup("slee/facilities/timer");
+
+			refreshTime = ((Long) ctx.lookup("RefreshTime")).longValue();
+			rssFeedString = (String) ctx.lookup("RSSFeedURL");
+
 		} catch (NamingException ne) {
 			tracer.severe("Could not set SBB context:", ne);
 		}
 
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.slee.Sbb#unsetSbbContext()
-	 */
-	public void unsetSbbContext() {
-		// TODO Auto-generated method stub
+	// CMP Fields
 
-	}
+	public abstract void setFeedHashCode(int feedHashCode);
+
+	public abstract int getFeedHashCode();
+
+	// Event handler methods
 
 	public void onStartServiceEvent(
 			javax.slee.serviceactivity.ServiceStartedEvent event,
 			ActivityContextInterface aci) {
-		
+
 		tracer.info("<><><><><> \n onStartServiceEvent of HttpClientRAExampleSbb \n <><><><><><><>");
-		
+
 		processRssFeed();
 
 		setTimer(aci);
-		
-		aci.detach(sbbContext.getSbbLocalObject());
+
 	}
 
 	private void processRssFeed() {
 		try {
-			Context myEnv = (Context) new InitialContext()
-					.lookup("java:comp/env");
-
-			String rssFeedString = (String) myEnv.lookup("RSSFeedURL");
 			URL rssFeedUrl = new URL(rssFeedString);
 
 			SyndFeedInput input = new SyndFeedInput();
@@ -224,22 +131,22 @@ public abstract class HttpClientRAExampleSbb implements javax.slee.Sbb {
 					tracer.info("Getting the Content for URL "
 							+ syndFeed.getLink());
 
-					HttpMethod httpMethod = raSbbInterface.createHttpMethod(
-							HttpMethodName.GET, syndFeed.getLink());
+					HttpGet httpGet = new HttpGet(syndFeed.getLink());
+
 					try {
 
 						HttpClientActivity clientActivity = raSbbInterface
-								.createHttpClientActivity(true);
+								.createHttpClientActivity(true, null);
 
 						ActivityContextInterface clientAci = httpClientAci
 								.getActivityContextInterface(clientActivity);
 						clientAci.attach(sbbContext.getSbbLocalObject());
 
-						clientActivity.executeMethod(httpMethod);
+						clientActivity.execute(httpGet, syndFeed.getLink());
 
 					} catch (Throwable e) {
-						tracer.severe("Error while creating HttpClientActivity",
-								e);
+						tracer.severe(
+								"Error while creating HttpClientActivity", e);
 					}
 				}
 
@@ -247,41 +154,19 @@ public abstract class HttpClientRAExampleSbb implements javax.slee.Sbb {
 				tracer.info("No new entry in the RSS Feed " + rssFeedString);
 			}
 
-		} catch (NamingException namingEx) {
-			tracer.severe("NamingException", namingEx);
-		} catch (MalformedURLException malformedEx) {
-			tracer.severe("MalformedURLException", malformedEx);
-		} catch (IOException ioEx) {
-			tracer.severe("IOException", ioEx);
-		} catch (FeedException feedEx) {
-			tracer.severe("FeedException", feedEx);
-
+		} catch (Exception e) {
+			tracer.severe("Failed to process RSS Feed", e);
 		}
 	}
 
 	private void setTimer(ActivityContextInterface aci) {
+
 		TimerOptions options = new TimerOptions();
 		options.setPreserveMissed(TimerPreserveMissed.ALL);
-		
-		TimerID timerID;
-		long refreshTime;
 
-		try {
-			Context initCtx = new InitialContext();
-			Context myEnv = (Context) initCtx.lookup("java:comp/env");
+		this.timerFacility.setTimer(aci, null, System.currentTimeMillis()
+				+ refreshTime, options);
 
-			refreshTime = ((Long) myEnv.lookup("RefreshTime")).longValue();
-
-			timerID = this.timerFacility.setTimer(aci, null, System
-					.currentTimeMillis()
-					+ refreshTime, options);
-
-			// Setting Timer ID
-			this.setTimerID(timerID);
-
-		} catch (NamingException e) {
-			tracer.severe(e.getMessage(), e);
-		}
 	}
 
 	public void onTimerEvent(TimerEvent event, ActivityContextInterface aci) {
@@ -295,21 +180,101 @@ public abstract class HttpClientRAExampleSbb implements javax.slee.Sbb {
 
 	public void onResponseEvent(ResponseEvent event,
 			ActivityContextInterface aci) {
-		Response response = event.getResponse();
-		
+		HttpResponse response = event.getHttpResponse();
 		tracer.info("********** onResponseEvent **************");
-		tracer.info("Status Code = "+response.getStatusCode());
-		tracer.info("Response Body = "+response.getResponseBodyAsString());
+		tracer.info("URI = " + event.getRequestApplicationData());
+		tracer.info("Status Code = " + response.getStatusLine().getStatusCode());
+		try {
+			tracer.info("Response Body = "
+					+ EntityUtils.toString(response.getEntity()));
+		} catch (Exception e) {
+			tracer.severe("Failed reading response body", e);
+		}
 		tracer.info("*****************************************");
 	}
 
-	// 'timerID' CMP field setter
-	public abstract void setTimerID(TimerID value);
+	// Unused SBB lifecycle methods
 
-	public abstract TimerID getTimerID();
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbActivate()
+	 */
+	public void sbbActivate() {
+	}
 
-	public abstract void setFeedHashCode(int feedHashCode);
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbCreate()
+	 */
+	public void sbbCreate() throws CreateException {
+	}
 
-	public abstract int getFeedHashCode();
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbExceptionThrown(java.lang.Exception,
+	 * java.lang.Object, javax.slee.ActivityContextInterface)
+	 */
+	public void sbbExceptionThrown(Exception arg0, Object arg1,
+			ActivityContextInterface arg2) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbLoad()
+	 */
+	public void sbbLoad() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbPassivate()
+	 */
+	public void sbbPassivate() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbPostCreate()
+	 */
+	public void sbbPostCreate() throws CreateException {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbRemove()
+	 */
+	public void sbbRemove() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbRolledBack(javax.slee.RolledBackContext)
+	 */
+	public void sbbRolledBack(RolledBackContext arg0) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#sbbStore()
+	 */
+	public void sbbStore() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.slee.Sbb#unsetSbbContext()
+	 */
+	public void unsetSbbContext() {
+	}
 
 }
