c6248d7fd57148fc51db9a6427d21492465c0f1a
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-11-16 16:29:26 +0000

    added listener for ForwardShortMessageRequest
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-slee@21055 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- examples/map-demo/sbb/src/main/java/org/mobicents/slee/examples/map/MapSbb.java
+++ examples/map-demo/sbb/src/main/java/org/mobicents/slee/examples/map/MapSbb.java
@@ -10,23 +10,25 @@ import javax.slee.SbbContext;
 import javax.slee.facilities.Tracer;
 
 import org.mobicents.protocols.ss7.map.api.MAPException;
+import org.mobicents.protocols.ss7.map.api.MAPParameterFactory;
 import org.mobicents.protocols.ss7.map.api.MAPProvider;
-import org.mobicents.protocols.ss7.map.api.MapServiceFactory;
+import org.mobicents.protocols.ss7.map.api.primitives.ISDNAddressString;
+import org.mobicents.protocols.ss7.map.api.primitives.USSDString;
 import org.mobicents.protocols.ss7.map.api.service.supplementary.MAPDialogSupplementary;
-import org.mobicents.protocols.ss7.map.api.service.supplementary.ProcessUnstructuredSSIndication;
-import org.mobicents.protocols.ss7.map.api.service.supplementary.USSDString;
-import org.mobicents.protocols.ss7.map.api.service.supplementary.UnstructuredSSIndication;
-import org.mobicents.slee.resource.map.DialogAccept;
-import org.mobicents.slee.resource.map.DialogClose;
-import org.mobicents.slee.resource.map.DialogDelimiter;
-import org.mobicents.slee.resource.map.DialogNotice;
-import org.mobicents.slee.resource.map.DialogProviderAbort;
-import org.mobicents.slee.resource.map.DialogReject;
-import org.mobicents.slee.resource.map.DialogRequest;
-import org.mobicents.slee.resource.map.DialogTimeout;
-import org.mobicents.slee.resource.map.DialogUserAbort;
-import org.mobicents.slee.resource.map.InvokeTimeout;
 import org.mobicents.slee.resource.map.MAPContextInterfaceFactory;
+import org.mobicents.slee.resource.map.events.DialogAccept;
+import org.mobicents.slee.resource.map.events.DialogClose;
+import org.mobicents.slee.resource.map.events.DialogDelimiter;
+import org.mobicents.slee.resource.map.events.DialogNotice;
+import org.mobicents.slee.resource.map.events.DialogProviderAbort;
+import org.mobicents.slee.resource.map.events.DialogReject;
+import org.mobicents.slee.resource.map.events.DialogRequest;
+import org.mobicents.slee.resource.map.events.DialogTimeout;
+import org.mobicents.slee.resource.map.events.DialogUserAbort;
+import org.mobicents.slee.resource.map.events.InvokeTimeout;
+import org.mobicents.slee.resource.map.events.service.sms.ForwardShortMessageRequest;
+import org.mobicents.slee.resource.map.events.service.suplementary.ProcessUnstructuredSSRequest;
+import org.mobicents.slee.resource.map.events.service.suplementary.UnstructuredSSRequest;
 
 /**
  * A simple example that listens for incoming MAP(USSD) Message and sends back
@@ -44,7 +46,7 @@ public abstract class MapSbb implements Sbb {
 
 	private MAPContextInterfaceFactory mapAcif;
 	private MAPProvider mapProvider;
-	private MapServiceFactory mapServiceFactory;
+	private MAPParameterFactory mapParameterFactory;
 
 	/** Creates a new instance of CallSbb */
 	public MapSbb() {
@@ -95,13 +97,13 @@ public abstract class MapSbb implements Sbb {
 			this.logger.info("Rx :  onDialogNotice" + evt);
 		}
 	}
-	
+
 	public void onDialogTimeout(DialogTimeout evt, ActivityContextInterface aci) {
 		if (logger.isInfoEnabled()) {
 			this.logger.info("Rx :  onDialogTimeout" + evt);
 		}
 	}
-	
+
 	public void onInvokeTimeout(InvokeTimeout evt, ActivityContextInterface aci) {
 		if (logger.isInfoEnabled()) {
 			this.logger.info("Rx :  onInvokeTimeout" + evt);
@@ -114,27 +116,24 @@ public abstract class MapSbb implements Sbb {
 		}
 	}
 
-	public void onProcessUnstructuredSSRequest(ProcessUnstructuredSSIndication evt, ActivityContextInterface aci) {
+	public void onProcessUnstructuredSSRequest(ProcessUnstructuredSSRequest evt, ActivityContextInterface aci) {
 		USSDString ussdStrObj = evt.getUSSDString();
 		String ussdStr = ussdStrObj.getString();
 		long invokeId = evt.getInvokeId();
 
 		if (this.logger.isInfoEnabled()) {
-			this.logger.info(String.format(
-					"Received PROCESS_UNSTRUCTURED_SS_REQUEST_INDICATION event USSDString=%s invokeId=%d", ussdStr,
-					invokeId));
+			this.logger.info(String.format("Received PROCESS_UNSTRUCTURED_SS_REQUEST_INDICATION event USSDString=%s invokeId=%d", ussdStr, invokeId));
 		}
 
 		this.setInvokeId(invokeId);
 
-		ussdStrObj = this.mapServiceFactory
-				.createUSSDString("USSD String : Hello World <CR> 1. Balance <CR> 2. Texts Remaining");
+		ussdStrObj = this.mapParameterFactory.createUSSDString("USSD String : Hello World <CR> 1. Balance <CR> 2. Texts Remaining");
 		byte ussdDataCodingScheme = (byte) 0x0F;
 		MAPDialogSupplementary dialog = evt.getMAPDialog();
 
 		try {
 			// FIXME: msisdn ?
-			dialog.addUnstructuredSSRequest(ussdDataCodingScheme, ussdStrObj);
+			dialog.addUnstructuredSSRequest(ussdDataCodingScheme, ussdStrObj, null, null);
 			dialog.send();
 		} catch (MAPException e) {
 			logger.severe("Error while sending UnstructuredSSRequest ", e);
@@ -142,30 +141,38 @@ public abstract class MapSbb implements Sbb {
 
 	}
 
-	public void onUnstructuredSSRequest(UnstructuredSSIndication evt, ActivityContextInterface aci) {
+	public void onUnstructuredSSRequest(UnstructuredSSRequest evt, ActivityContextInterface aci) {
 		USSDString ussdStrObj = evt.getUSSDString();
 		String ussdStr = ussdStrObj.getString();
 		long invokeId = evt.getInvokeId();
 
 		if (this.logger.isInfoEnabled()) {
-			this.logger.info(String.format(
-					"Received UNSTRUCTURED_SS_REQUEST_INDICATION event USSDString=%s invokeId=%d", ussdStr, invokeId));
+			this.logger.info(String.format("Received UNSTRUCTURED_SS_REQUEST_INDICATION event USSDString=%s invokeId=%d", ussdStr, invokeId));
 		}
 
-		ussdStrObj = this.mapServiceFactory.createUSSDString("Your balance = 350");
+		ussdStrObj = this.mapParameterFactory.createUSSDString("Your balance = 350");
 		byte ussdDataCodingScheme = (byte) 0x0F;
 		MAPDialogSupplementary dialog = evt.getMAPDialog();
 
 		try {
 			// FIXME: msisdn ?
-			dialog.addProcessUnstructuredSSResponse(this.getInvokeId(), true, ussdDataCodingScheme, ussdStrObj);
+			dialog.addProcessUnstructuredSSResponse(this.getInvokeId(), ussdDataCodingScheme, ussdStrObj);
 			dialog.close(false);
 		} catch (MAPException e) {
 			logger.severe("Error while sending UnstructuredSSRequest ", e);
 		}
 
 	}
-	
+
+	public void onForwardShortMessageRequest(ForwardShortMessageRequest evt, ActivityContextInterface aci) {
+		ISDNAddressString isdn = evt.getSM_RP_OA().getMsisdn();
+		byte[] sms = evt.getSM_RP_UI();
+
+		if (this.logger.isInfoEnabled()) {
+			this.logger.info(String.format("Received FORWARD_SHORT_MESSAGE_REQUEST event=%s", evt.toString()));
+		}
+	}
+
 	/**
 	 * Life cycle methods
 	 */
@@ -180,14 +187,14 @@ public abstract class MapSbb implements Sbb {
 
 			mapProvider = (MAPProvider) ctx.lookup("slee/resources/map/2.0/provider");
 
-			this.mapServiceFactory = this.mapProvider.getMapServiceFactory();
+			this.mapParameterFactory = this.mapProvider.getMAPParameterFactory();
 
 			this.logger = sbbContext.getTracer("MapTest");
 		} catch (Exception ne) {
 			logger.severe("Could not set SBB context:", ne);
 		}
 
-	}	
+	}
 
 	public void unsetSbbContext() {
 		this.sbbContext = null;
@@ -223,7 +230,7 @@ public abstract class MapSbb implements Sbb {
 	}
 
 	/**
-	 * CMP's 
+	 * CMP's
 	 */
 	public abstract void setInvokeId(long invokeId);
 
