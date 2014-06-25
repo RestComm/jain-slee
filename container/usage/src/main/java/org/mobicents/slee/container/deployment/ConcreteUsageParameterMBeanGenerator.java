f96fe60690807abb53f51926d61942c033ea48ac
emmartins <emartins@redhat.com>
2014-06-26 00:50:11 +0100

    updating copyright of changed classes

--- container/usage/src/main/java/org/mobicents/slee/container/deployment/ConcreteUsageParameterMBeanGenerator.java
+++ container/usage/src/main/java/org/mobicents/slee/container/deployment/ConcreteUsageParameterMBeanGenerator.java
@@ -1,36 +1,22 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2011, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
-
-/*
- * Created on Mar 14, 2005
- * 
- * The Open SLEE Project
- * 
- * A SLEE for the People
- * 
- * The source code contained in this file is in in the public domain.          
- * It can be used in any project or product without prior permission,         
- * license or royalty payments. There is no claim of correctness and
- * NO WARRANTY OF ANY KIND provided with this code.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * This file incorporates work covered by the following copyright contributed under the GNU LGPL : Copyright 2007-2011 Red Hat.
  */
 package org.mobicents.slee.container.deployment;
 
