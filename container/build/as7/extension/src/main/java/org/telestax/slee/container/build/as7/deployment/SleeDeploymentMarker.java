059ffea4e1ce98d8546c9972dd41ea805c6c4511
SergeyLee <sergey.povarnin@mobius-software.com>
2016-09-12 15:17:11 +0300

    Deployment parsing sources and resorces.

--- container/build/as7/extension/src/main/java/org/telestax/slee/container/build/as7/deployment/SleeDeploymentMarker.java
+++ container/build/as7/extension/src/main/java/org/telestax/slee/container/build/as7/deployment/SleeDeploymentMarker.java
@@ -1,9 +1,14 @@
 package org.telestax.slee.container.build.as7.deployment;
 
-import java.net.URL;
-
 import org.jboss.as.server.deployment.AttachmentKey;
+import org.jboss.as.server.deployment.Attachments;
 import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.module.ResourceRoot;
+import org.jboss.logging.Logger;
+import org.jboss.vfs.VFSUtils;
+import org.jboss.vfs.VirtualFile;
+
+import java.net.URL;
 
 /**
  *
@@ -12,13 +17,40 @@ import org.jboss.as.server.deployment.DeploymentUnit;
  */
 public class SleeDeploymentMarker {
 
-    private static final AttachmentKey<URL> ATTACHMENT_KEY = AttachmentKey.create(URL.class);
+    static Logger log = Logger.getLogger(SleeDeploymentMarker.class);
 
-    public static void setDeployableUnitJarURL(final DeploymentUnit deployment, final URL duURL) {
-        deployment.putAttachment(ATTACHMENT_KEY, duURL);
+    private static final AttachmentKey<URL> URL_KEY = AttachmentKey.create(URL.class);
+    private static final AttachmentKey<SleeDeploymentMetaData> SLEEMETADATA_KEY = AttachmentKey.create(SleeDeploymentMetaData.class);
+
+    public static void mark(final DeploymentUnit deploymentUnit) {
+        final ResourceRoot deploymentRoot = deploymentUnit.getAttachment(Attachments.DEPLOYMENT_ROOT);
+        final VirtualFile root = deploymentRoot.getRoot();
+        URL rootURL = null;
+        try {
+            rootURL = VFSUtils.getRootURL(root);
+        }
+        catch (Exception ex) {
+            log.error("Cannot get URL for deployable unit: " + ex.getLocalizedMessage());
+        }
+        setDeployableUnitJarURL(deploymentUnit, rootURL);
+
+        SleeDeploymentMetaData metaData = new SleeDeploymentMetaData(deploymentUnit);
+        setDeployableUnitJarMetaData(deploymentUnit, metaData);
+    }
+
+    public static void setDeployableUnitJarURL(final DeploymentUnit deploymentUnit, final URL duURL) {
+        deploymentUnit.putAttachment(URL_KEY, duURL);
     }
 
     public static URL getDeployableUnitJarURL(final DeploymentUnit deploymentUnit) {
-        return deploymentUnit.getAttachment(ATTACHMENT_KEY);        
+        return deploymentUnit.getAttachment(URL_KEY);
+    }
+
+    public static void setDeployableUnitJarMetaData(final DeploymentUnit deploymentUnit, final SleeDeploymentMetaData duMetaData) {
+        deploymentUnit.putAttachment(SLEEMETADATA_KEY, duMetaData);
+    }
+
+    public static SleeDeploymentMetaData getDeployableUnitJarMetaData(final DeploymentUnit deploymentUnit) {
+        return deploymentUnit.getAttachment(SLEEMETADATA_KEY);
     }
 }
--- container/build/as7/extension/src/main/java/org/telestax/slee/container/build/as7/deployment/SleeDeploymentParseProcessor.java
+++ container/build/as7/extension/src/main/java/org/telestax/slee/container/build/as7/deployment/SleeDeploymentParseProcessor.java
@@ -1,16 +1,11 @@
 package org.telestax.slee.container.build.as7.deployment;
 
-import java.util.Locale;
-
-import org.jboss.as.server.deployment.Attachments;
-import org.jboss.as.server.deployment.DeploymentPhaseContext;
-import org.jboss.as.server.deployment.DeploymentUnit;
-import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
-import org.jboss.as.server.deployment.DeploymentUnitProcessor;
-import org.jboss.as.server.deployment.Phase;
+import org.jboss.as.server.deployment.*;
 import org.jboss.as.server.deployment.module.ResourceRoot;
 import org.jboss.logging.Logger;
 import org.jboss.vfs.VirtualFile;
+
+import java.util.Locale;
 
 public class SleeDeploymentParseProcessor implements DeploymentUnitProcessor {
 
@@ -30,27 +25,25 @@ public class SleeDeploymentParseProcessor implements DeploymentUnitProcessor {
 
     @Override
     public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+
         final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
         final ResourceRoot deploymentRoot = deploymentUnit.getAttachment(Attachments.DEPLOYMENT_ROOT);
+
         if(!deploymentRoot.getRootName().toLowerCase(Locale.ENGLISH).endsWith(".jar")) {
             return;
         }
+
         final VirtualFile descriptor = deploymentRoot.getRoot().getChild("META-INF/deployable-unit.xml");
         if (descriptor == null) {
             return;
         }
+
+
+
         // TODO mark deployment as SLEE by storing the jar URL
-        //SleeDeploymentMarker.mark(deploymentUnit);
-        // deploy
-        try {
-            log.info("deploying "+deploymentUnit.getName());
-            log.info("deploymentRoot root name: "+deploymentRoot.getRootName());
-            log.info("deploymentRoot canonical path: "+deploymentRoot.getRoot().getPhysicalFile().getCanonicalPath());
-            log.info("deploymentRoot parent canonical path: "+deploymentRoot.getRoot().getParent().getPhysicalFile().getCanonicalPath());
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        SleeDeploymentMarker.mark(deploymentUnit);
+        //SleeDeploymentMarker.setDeployableUnitJarURL(deploymentUnit, rootURL);
+        //SleeDeploymentMarker.setDeployableUnitJarMetaData(deploymentUnit, metaData);
     }
 
     @Override
