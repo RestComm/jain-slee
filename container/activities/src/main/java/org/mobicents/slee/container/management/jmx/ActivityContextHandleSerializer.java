9bcde2884b062c2be4bbdad2df1bd76b2c23b6f7
SergeyLee <sergey.povarnin@mobius-software.com>
2016-12-01 17:12:41 +0200

    Replaced JBoss Serialization org.jboss.serial.io.JBossObject* with Apache Commons SerializationUtils.clone() method.

--- container/activities/src/main/java/org/mobicents/slee/container/management/jmx/ActivityContextHandleSerializer.java
+++ container/activities/src/main/java/org/mobicents/slee/container/management/jmx/ActivityContextHandleSerializer.java
@@ -24,11 +24,13 @@ package org.mobicents.slee.container.management.jmx;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import javax.slee.resource.ActivityHandle;
 
-import org.jboss.serial.io.JBossObjectInputStream;
-import org.jboss.serial.io.JBossObjectOutputStream;
+//import org.jboss.serial.io.JBossObjectInputStream;
+//import org.jboss.serial.io.JBossObjectOutputStream;
 import org.jboss.util.Base64;
 import org.mobicents.slee.container.SleeContainer;
 import org.mobicents.slee.container.activity.ActivityContextHandle;
@@ -42,78 +44,75 @@ import org.mobicents.slee.runtime.facilities.nullactivity.NullActivityHandleImpl
 
 public class ActivityContextHandleSerializer {
 
-  public static JmxActivityContextHandle encode(ActivityContextHandle ach) {
-    return new JmxActivityContextHandle(ach.getActivityType(), ach.getActivityType() == ActivityType.RA ? ((ResourceAdaptorActivityContextHandle)ach).getResourceAdaptorEntity().getName() : "", handleToString(ach.getActivityHandle()), ach.getActivityHandle().toString());
-  }
-
-  public static ActivityContextHandle decode(JmxActivityContextHandle jmxach, SleeContainer sleeContainer) {
-	 final ActivityHandle activityHandle = StringToHandle(jmxach.getActivityHandleBase64());
-	  switch (jmxach.getActivityType()) {
-	
-	  case RA:
-		final String raEntityName = jmxach.getActivitySource();
-		final ResourceAdaptorEntity raEntity = sleeContainer.getResourceManagement().getResourceAdaptorEntity(raEntityName);
-		if (raEntity != null) {
-			return raEntity.getActivityContextHandle(activityHandle);
-		}
-		else {
-			throw new IllegalArgumentException("unable to recreate activity context handle, the ra entity not found");
-		}
-		
-	  case PTABLE:
-		return ((ProfileTableActivityHandle)activityHandle).getActivityContextHandle();
-		
-	  case SERVICE:
-		return ((ServiceActivityHandle)activityHandle).getActivityContextHandle();
-			
-	  case NULL:
-		return new NullActivityContextHandle((NullActivityHandleImpl) activityHandle);
-			
-	  default:
-		throw new IllegalArgumentException("unexpected activity type");		
-	} 
-  }
-
-  private static String handleToString(ActivityHandle ah) {
-    String base64 = null;
-    try {
-      byte[] bytes = null;
-      // need to marshall the handle, but this happens at most once per AS instance
-      int bufferSize = 1024;
-      final ByteArrayOutputStream baos =  new ByteArrayOutputStream(bufferSize);
-      final JBossObjectOutputStream jboos = new JBossObjectOutputStream(baos);
-
-      jboos.writeObject(ah);
-
-      bytes = baos.toByteArray();
-
-      base64 = Base64.encodeBytes(bytes);
-      jboos.close();
-    }
-    catch (Exception e) {
-      e.printStackTrace();
+    public static JmxActivityContextHandle encode(ActivityContextHandle ach) {
+        return new JmxActivityContextHandle(ach.getActivityType(), ach.getActivityType() == ActivityType.RA ? ((ResourceAdaptorActivityContextHandle) ach).getResourceAdaptorEntity().getName() : "", handleToString(ach.getActivityHandle()), ach.getActivityHandle().toString());
     }
 
-    return base64;
-  }
+    public static ActivityContextHandle decode(JmxActivityContextHandle jmxach, SleeContainer sleeContainer) {
+        final ActivityHandle activityHandle = StringToHandle(jmxach.getActivityHandleBase64());
+        switch (jmxach.getActivityType()) {
 
-  private static ActivityHandle StringToHandle(String base64) {
+            case RA:
+                final String raEntityName = jmxach.getActivitySource();
+                final ResourceAdaptorEntity raEntity = sleeContainer.getResourceManagement().getResourceAdaptorEntity(raEntityName);
+                if (raEntity != null) {
+                    return raEntity.getActivityContextHandle(activityHandle);
+                } else {
+                    throw new IllegalArgumentException("unable to recreate activity context handle, the ra entity not found");
+                }
 
-    ActivityHandle activityHandle = null;
+            case PTABLE:
+                return ((ProfileTableActivityHandle) activityHandle).getActivityContextHandle();
 
-    try {
-      byte[] bytes = Base64.decode(base64);
+            case SERVICE:
+                return ((ServiceActivityHandle) activityHandle).getActivityContextHandle();
 
-      final ByteArrayInputStream bais =  new ByteArrayInputStream(bytes);
-      final JBossObjectInputStream jbois = new JBossObjectInputStream(bais);
+            case NULL:
+                return new NullActivityContextHandle((NullActivityHandleImpl) activityHandle);
 
-      activityHandle = (ActivityHandle) jbois.readObject();
+            default:
+                throw new IllegalArgumentException("unexpected activity type");
+        }
     }
-    catch (Exception e) {
-      e.printStackTrace();
+
+    private static String handleToString(ActivityHandle ah) {
+        String base64 = null;
+        try {
+            byte[] bytes = null;
+            // need to marshall the handle, but this happens at most once per AS instance
+            int bufferSize = 1024;
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream(bufferSize);
+            //final JBossObjectOutputStream jboos = new JBossObjectOutputStream(baos);
+            final ObjectOutputStream oos = new ObjectOutputStream(baos);
+
+            oos.writeObject(ah);
+            bytes = baos.toByteArray();
+
+            base64 = Base64.encodeBytes(bytes);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return base64;
     }
 
-    return activityHandle;
-  }
+    private static ActivityHandle StringToHandle(String base64) {
+        ActivityHandle activityHandle = null;
+        try {
+            byte[] bytes = Base64.decode(base64);
+
+            final ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            //final JBossObjectInputStream jbois = new JBossObjectInputStream(bais);
+
+            final ObjectInputStream ois = new ObjectInputStream(bais);
+
+            activityHandle = (ActivityHandle) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return activityHandle;
+    }
 
 }
