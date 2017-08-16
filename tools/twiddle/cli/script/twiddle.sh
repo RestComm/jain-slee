85ac5a716861586cf616ec6c53745e3e182eda09
Anatoly Satanovskiy <anatoly.satanovskiy@mobius-software.com>
2017-08-16 13:31:20 +0300

    modified twiddle sh script
    # Conflicts:
    #	tools/twiddle/cli/script/twiddle.sh

--- tools/twiddle/cli/script/twiddle.sh
+++ tools/twiddle/cli/script/twiddle.sh
@@ -17,18 +17,8 @@
 # Enable the remoting-jmx protocol by adapting the TWIDDLE_CLASSPATH to utilise JBoss AS7 / WildFly modules.
 #
 
-# JBoss AS7
-#JBOSS_MODULEPATH=$JBOSS_HOME/modules
-#MODULES="org/jboss/remoting3/remoting-jmx org/jboss/remoting3 org/jboss/logging org/jboss/xnio org/jboss/xnio/nio org/jboss/sasl org/jboss/marshalling org/jboss/marshalling/river org/jboss/as/cli org/jboss/staxmapper org/jboss/as/protocol org/jboss/dmr org/jboss/as/controller-client org/jboss/threads org/jboss/as/controller"
-#TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$JBOSS_HOME/bin/client/jboss-client.jar"
-
-# JBoss AS72
 JBOSS_MODULEPATH=$JBOSS_HOME/modules/system/layers/base/
-MODULES="org/jboss/remoting-jmx org/jboss/remoting3 org/jboss/logging org/jboss/xnio org/jboss/xnio/nio org/jboss/sasl org/jboss/marshalling org/jboss/marshalling/river org/jboss/as/cli org/jboss/staxmapper org/jboss/as/protocol org/jboss/dmr org/jboss/as/controller-client org/jboss/threads org/jboss/as/controller org/telestax/slee/container/lib"
-# WildFly
-#JBOSS_MODULEPATH=$JBOSS_HOME/modules/system/layers/base/
-#MODULES="org/jboss/remoting-jmx org/jboss/remoting org/jboss/logging org/jboss/xnio org/jboss/xnio/nio org/jboss/sasl org/jboss/marshalling org/jboss/marshalling/river org/jboss/as/cli org/jboss/staxmapper org/jboss/as/protocol org/jboss/dmr org/jboss/as/controller-client org/jboss/threads org/wildfly/security/manager org/telestax/slee/container/lib" 
-
+MODULES="org/jboss/remoting-jmx org/jboss/logging org/jboss/xnio org/jboss/xnio/nio org/jboss/sasl org/jboss/marshalling org/jboss/marshalling/river org/jboss/as/cli org/jboss/staxmapper org/jboss/as/protocol org/jboss/dmr org/jboss/as/controller-client org/jboss/threads org/jboss/as/controller org/telestax/slee/container/lib"
 for MODULE in $MODULES
 do
     for JAR in `cd "$JBOSS_MODULEPATH/$MODULE/main/" && ls -1 *.jar`
@@ -36,7 +26,6 @@ do
         TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$JBOSS_MODULEPATH/$MODULE/main/$JAR"
     done
 done
-
 
 # Extract the directory and the program name
 # takes care of symlinks
@@ -81,13 +70,6 @@ if $cygwin ; then
         JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
 fi
 
-# Setup TWIDDLE_HOME
-if [ "x$TWIDDLE_HOME" = "x" ]; then
-    #TWIDDLE_HOME=`cd $DIRNAME/..; pwd`
-    TWIDDLE_HOME=$DIRNAME
-fi
-export TWIDDLE_HOME
-
 # Setup the JVM
 if [ "x$JAVA_HOME" != "x" ]; then
     JAVA=$JAVA_HOME/bin/java
@@ -95,65 +77,16 @@ else
     JAVA="java"
 fi
 
-#debug
-#JAVA_OPTS="$JAVA_OPTS -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y"
-
 # Setup the classpath
+TWIDDLE_HOME=$JSLEE_HOME/tools/twiddle
+TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/twiddle.jar"
+TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jbossall-client.jar"
+TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jboss-jmx.jar"
+TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/cli-twiddle.jar"
+TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib"
+TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$JBOSS_HOME/bin/client/jboss-client.jar"
 
-TWIDDLE_BOOT_CLASSPATH="$TWIDDLE_HOME/lib/twiddle.jar"
-
-#if [ "x$TWIDDLE_CLASSPATH" = "x" ]; then
-#    TWIDDLE_CLASSPATH="$TWIDDLE_BOOT_CLASSPATH"
-    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_BOOT_CLASSPATH"
-    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jbossall-client.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jboss-logging.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/log4j.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/dom4j.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/getopt.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jboss-common-core.jar"    
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jbosssx-client.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jboss-jmx.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jnp-client.jar"
-    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/cli-twiddle.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jain-slee-1.1.jar"
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib/jmx-property-editors.jar"
-    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_HOME/lib"
-#else
-#    TWIDDLE_CLASSPATH="$TWIDDLE_CLASSPATH:$TWIDDLE_BOOT_CLASSPATH"
-#fi
-
-#if [ "x$JBOSS_CLASSPATH" = "x" ]; then
-
- 	#jboss
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/twiddle.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/getopt.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/log4j.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jboss-jmx.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jboss-common-client.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jmx-invoker-adaptor-client.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jmx-client.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jnp-client.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jboss-serialization.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jboss-minimal.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/javaee-api.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jboss-security-spi.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jboss-transaction-spi.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/concurrent.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/dom4j.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jbossx-security-client.jar"
-    
-    #restcomm
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/cli-twiddle.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jain-slee-1.1.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/jmx-property-editors.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/activities.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib/spi.jar"
-    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:${DIRNAME}/lib"
-   
-#fi
-
-SLEE_TWIDDLE_CONF="$DIRNAME/lib/slee-twiddle.properties"
-#SLEE_TWIDDLE_CONF="slee-twiddle.properties"
+SLEE_TWIDDLE_CONF="$TWIDDLE_HOME/lib/slee-twiddle.properties"
 
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
@@ -164,39 +97,6 @@ if $cygwin; then
 	SLEE_TWIDDLE_CONF=`cygpath --path --windows "$SLEE_TWIDDLE_CONF"`
 fi
 
-# Display our environment
-#     echo "========================================================================="
-#     echo ""
-#     echo "  CLI Bootstrap Environment"
-#     echo ""
-#     echo "  DIRNAME  : $DIRNAME"
-#     echo ""
-#     echo "  JAVA     : $JAVA"
-#     echo ""
-#     echo "  JAVA_OPTS: $JAVA_OPTS"
-#     echo ""
-#     echo "  JBOSS_CLASSPATH: $JBOSS_CLASSPATH"
-#     echo ""
-#     echo "  TWIDDLE_HOME: $TWIDDLE_HOME"
-#     echo ""
-#     echo "  TWIDDLE_CLASSPATH: $TWIDDLE_CLASSPATH"
-#     echo ""
-#     echo "  OPTS     : $*"
-#     echo ""
-#     echo "  CONF     : $SLEE_TWIDDLE_CONF"
-#     echo ""
-#     echo "========================================================================="
-#     echo ""
-
-# Execute the JVM
-#exec "$JAVA" \
-#    $JAVA_OPTS \
-#    -Dprogram.name="$PROGNAME" \
-#    -classpath $JBOSS_CLASSPATH \
-#    org.jboss.console.twiddle.Twiddle -c file:///$SLEE_TWIDDLE_CONF $@
-
-echo "classpash: $TWIDDLE_CLASSPATH"
-    
 exec "$JAVA" \
     $JAVA_OPTS \
     -Dprogram.name="$PROGNAME" \
